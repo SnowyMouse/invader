1b1428bd3e74609814a512bd35b5043d474ad425
Kavawuvi <flycoba1tdragon@gmail.com>
2019-05-31 20:44:31 -0500

    Correct grenade throwing velocity

--- src/tag/hek/class/actor_variant.cpp
+++ src/tag/hek/class/actor_variant.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+#include "../../../hek/constants.hpp"
 #include "../compile.hpp"
 
 #include "actor_variant.hpp"
@@ -18,6 +19,7 @@ namespace Invader::HEK {
         ADD_DEPENDENCY_ADJUST_SIZES(tag.major_variant)
         ADD_DEPENDENCY_ADJUST_SIZES(tag.weapon)
         ADD_DEPENDENCY_ADJUST_SIZES(tag.equipment)
+        tag.grenade_velocity = tag.grenade_velocity.read() / TICK_RATE;
         ADD_REFLEXIVE(tag.change_colors)
         FINISH_COMPILE
     }
