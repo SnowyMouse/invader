6432dae8e5a1c044c7bf14bb66429bf9df3e9cc2
Kavawuvi <flycoba1tdragon@gmail.com>
2019-07-31 03:04:34 -0500

    Don't check leaves for now

--- src/tag/hek/class/model_collision_geometry.cpp
+++ src/tag/hek/class/model_collision_geometry.cpp
@@ -58,56 +58,7 @@ namespace Invader::HEK {
             return point.distance_from_plane(planes[plane_index].plane) >= 0;
         };
 
-        auto point_in_front_of_surface = [&surfaces, &surface_count, &point_in_front_of_plane](std::uint32_t surface_index) {
-            // Make sure it's a valid index
-            if(surface_index >= surface_count) {
-                eprintf("Invalid surface %u / %u in BSP.\n", surface_index, surface_count);
-                throw OutOfBoundsException();
-            }
-
-            return point_in_front_of_plane(surfaces[surface_index].plane.read().int_value());
-        };
-
-        auto point_in_bsp2d_reference = [&bsp2d_references, &bsp2d_reference_count, &point_in_front_of_plane, &point_in_front_of_surface](std::uint32_t bsp2d_index, std::uint32_t bsp2d_count) -> bool {
-            // Make sure the BSP2D count > 0 before doing anything.
-            if(bsp2d_count > 0) {
-                // Make sure it's a valid index
-                std::uint32_t bsp2d_end = bsp2d_index + bsp2d_count;
-                if((bsp2d_index > bsp2d_reference_count) || (bsp2d_end > bsp2d_reference_count)) {
-                    eprintf("Invalid BSP2D reference range (%u - %u) / %u in BSP.\n", bsp2d_index, bsp2d_end, bsp2d_reference_count);
-                    throw OutOfBoundsException();
-                }
-
-                for(std::uint32_t i = bsp2d_index; i < bsp2d_end; i++) {
-                    auto plane = bsp2d_references[i].plane.read();
-                    auto plane_index = plane.int_value();
-                    if(plane.flag_value()) {
-                        if(!point_in_front_of_surface(plane_index)) {
-                            return false;
-                        }
-                    }
-                    else if(!point_in_front_of_plane(plane_index)) {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        };
-
-        auto point_in_leaf = [&leaves, &leaf_count, &point_in_bsp2d_reference](std::uint32_t leaf_index) -> bool {
-            // Make sure it's a valid index
-            if(leaf_index >= leaf_count) {
-                eprintf("Invalid leaf %u / %u in BSP.\n", leaf_index, leaf_count);
-                throw OutOfBoundsException();
-            }
-
-            // Get the leaf
-            auto &leaf = leaves[leaf_index];
-            return point_in_bsp2d_reference(leaf.first_bsp2d_reference, leaf.bsp2d_reference_count);
-        };
-
-        auto point_in_3d_tree = [&bsp3d_nodes, &bsp3d_node_count, &point_in_front_of_plane, &point_in_leaf](FlaggedInt<std::uint32_t> node_index, auto &point_in_tree_recursion) -> bool {
+        auto point_in_3d_tree = [&bsp3d_nodes, &bsp3d_node_count, &point_in_front_of_plane](FlaggedInt<std::uint32_t> node_index, auto &point_in_tree_recursion) -> bool {
             while(true) {
                 // First, make sure it's not null
                 if(node_index.is_null()) {
@@ -116,7 +67,7 @@ namespace Invader::HEK {
 
                 // Next, make sure it's not a leaf. If it is, we should look at leaves, instead.
                 if(node_index.flag_value()) {
-                    return point_in_leaf(node_index);
+                    return true;
                 }
 
                 // Make sure it's a valid index
