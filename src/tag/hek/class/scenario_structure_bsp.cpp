8941f071cf02e03a5f72dae11cfca306d49aa485
Kavawuvi <flycoba1tdragon@gmail.com>
2019-08-16 14:58:22 -0500

    Don't set this value to 1 if there are no instances

--- src/tag/hek/class/scenario_structure_bsp.cpp
+++ src/tag/hek/class/scenario_structure_bsp.cpp
@@ -118,7 +118,7 @@ namespace Invader::HEK {
             ADD_REFLEXIVE(reflexive.instances);
             ADD_REFLEXIVE(reflexive.counts);
             ADD_REFLEXIVE(reflexive.z_reference_vectors);
-            reflexive.bullshit = 1;
+            reflexive.bullshit = reflexive.instances.count.read() != 0;
         } ADD_REFLEXIVE_END
 
         skip_data = true;
