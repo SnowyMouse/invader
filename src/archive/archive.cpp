86e80e52949d82b95f30629fd08001ebe8a42225
Kavawuvi <flycoba1tdragon@gmail.com>
2019-08-18 02:31:23 -0500

    Add the rest of invader-archive

--- src/archive/archive.cpp
+++ src/archive/archive.cpp
@@ -8,20 +8,22 @@
 #include <string>
 #include <getopt.h>
 #include <filesystem>
+#include <archive.h>
+#include <archive_entry.h>
 #include "../eprintf.hpp"
 
 #include "../build/build_workload.hpp"
 #include "../map/map.hpp"
 
 int main(int argc, const char **argv) {
-    if(argc < 3) {
-        eprintf("Usage: %s <scenario> <tags> [tags2 [...]]\n", argv[0]);
+    if(argc < 4) {
+        eprintf("Usage: %s <scenario> <output> <tags> [tags2 [...]]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     // Add tags to the array
     std::vector<std::string> tags;
-    for(int i = 2; i < argc; i++) {
+    for(int i = 3; i < argc; i++) {
         tags.emplace_back(argv[i]);
     }
 
@@ -34,7 +36,9 @@ int main(int argc, const char **argv) {
 
     // Go through each tag and see if we can find everything.
     std::vector<std::string> archive_list;
+    std::vector<std::string> archive_list_internal;
     archive_list.reserve(tag_count);
+    archive_list_internal.reserve(tag_count);
     for(std::size_t i = 0; i < tag_count; i++) {
         // Get the tag path information
         auto &tag = parsed_map.get_tag(i);
@@ -54,14 +58,58 @@ int main(int argc, const char **argv) {
             if(std::filesystem::exists(tag_path)) {
                 exists = true;
                 archive_list.push_back(tag_path.string());
+                archive_list_internal.push_back(full_tag_path);
                 break;
             }
         }
 
         if(!exists) {
-            eprintf("Failed to find %s.\n", full_tag_path.data());
+            eprintf("Failed to find %s. Archive could not be made.\n", full_tag_path.data());
+            return EXIT_FAILURE;
         }
     }
+
+    // Begin making the archive
+    auto *archive = archive_write_new();
+    archive_write_add_filter_xz(archive);
+    archive_write_set_format_pax_restricted(archive);
+    archive_write_open_filename(archive, argv[2]);
+
+    // Go through each tag path we got
+    for(std::size_t i = 0; i < tag_count; i++) {
+        // Begin
+        auto *entry = archive_entry_new();
+        archive_entry_set_pathname(entry, archive_list_internal[i].data());
+        archive_entry_set_perm(entry, 0644);
+        archive_entry_set_filetype(entry, AE_IFREG);
+
+        // Open the tag
+        std::FILE *t = std::fopen(archive_list[i].data(), "rb");
+        if(!t) {
+            eprintf("Failed to open %s for reading.\n", archive_list[i].data());
+            return EXIT_FAILURE;
+        }
+        std::fseek(t, 0, SEEK_END);
+        long size = std::ftell(t);
+        archive_entry_set_size(entry, size);
+        std::fseek(t, 0, SEEK_SET);
+
+        // Make a pointer and read the data
+        std::unique_ptr<std::byte[]> data = std::make_unique<std::byte[]>(size);
+        std::fread(data.get(), size, 1, t);
+        std::fclose(t);
+
+        // Archive that bastard
+        archive_write_header(archive, entry);
+        archive_write_data(archive, data.get(), size);
+
+        // Close it
+        archive_entry_free(entry);
+    }
+
+    // Save and close
+    archive_write_close(archive);
+    archive_write_free(archive);
 
     return EXIT_SUCCESS;
 }
