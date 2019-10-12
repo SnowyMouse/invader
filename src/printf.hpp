ef024a77854d021ef0047dc1ced8819a16cee2c7
Kavawuvi <flycoba1tdragon@gmail.com>
2019-10-11 19:28:57 -0500

    Fix unused warnings warnings when using NO_OUTPUT

--- src/printf.hpp
+++ src/printf.hpp
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#ifndef INVADER__EPRINTF_HPP
-#define INVADER__EPRINTF_HPP
+#ifndef INVADER__PRINTF_HPP
+#define INVADER__PRINTF_HPP
 
 #ifndef NO_OUTPUT
+
+// If NO_OUTPUT is not enabled, then we have eprintf and oprintf as a macro for, basically, printf
 #include <cstdio>
 #define eprintf(...) std::fprintf(stderr, __VA_ARGS__)
 #define oprintf(...) std::fprintf(stdout, __VA_ARGS__)
+
 #else
-#define eprintf(...) {}
-#define oprintf(...) {}
+
+// Otherwise, we have eprintf and oprintf as an inline, variadic function that does nothing. This is so we don't get any unused variable warnings.
+static inline void eprintf(...) {}
+static inline void oprintf(...) {}
 #endif
+
 #endif
