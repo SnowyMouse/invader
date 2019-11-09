88fa4114bed044fd60a05c4eb1e9742ac9c68d97
Kavawuvi <kavawuvi@tutanota.com>
2019-11-09 15:37:14 -0600

    Fix a typo

--- src/resource/resource.cmake
+++ src/resource/resource.cmake
@@ -4,10 +4,9 @@ if(NOT DEFINED ${INVADER_RESOURCE})
     set(INVADER_RESOURCE true CACHE BOOL "Build invader-resource (builds resource map files)")
 endif()
 
-if(${INVADER_DEPENDENCY})
+if(${INVADER_RESOURCE})
     add_executable(invader-resource
         src/resource/resource.cpp
     )
-
     target_link_libraries(invader-resource invader)
 endif()
