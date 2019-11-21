ec8e89e584eb47cacc55df6341e47b4cdebe8187
Kavawuvi <kavawuvi@tutanota.com>
2019-11-20 21:00:13 -0600

    Rename unused parameters

--- src/tag/parser/post_parse_cache_file_data.cpp
+++ src/tag/parser/post_parse_cache_file_data.cpp
@@ -22,13 +22,13 @@ namespace Invader::Parser {
         throw std::exception();
     }
 
-    void Invader::Parser::GlobalsFallingDamage::post_parse_cache_file_data(Invader::Tag const &tag, std::optional<HEK::Pointer> pointer) {
+    void Invader::Parser::GlobalsFallingDamage::post_parse_cache_file_data(Invader::Tag const &, std::optional<HEK::Pointer>) {
         this->maximum_falling_distance = static_cast<float>((this->maximum_falling_distance * this->maximum_falling_distance) / GRAVITY / 2.0f);
         this->harmful_falling_velocity.from = static_cast<float>((this->harmful_falling_velocity.from * this->harmful_falling_velocity.from) / GRAVITY / 2.0f);
         this->harmful_falling_velocity.to = static_cast<float>((this->harmful_falling_velocity.to * this->harmful_falling_velocity.to) / GRAVITY / 2.0f);
     }
 
-    void Invader::Parser::Glow::post_parse_cache_file_data(Invader::Tag const &tag, std::optional<HEK::Pointer> pointer) {
+    void Invader::Parser::Glow::post_parse_cache_file_data(Invader::Tag const &, std::optional<HEK::Pointer>) {
         this->attachment_0 = static_cast<HEK::FunctionOut>(this->attachment_0 + 1);
         this->attachment_1 = static_cast<HEK::FunctionOut>(this->attachment_1 + 1);
         this->attachment_2 = static_cast<HEK::FunctionOut>(this->attachment_2 + 1);
@@ -37,17 +37,17 @@ namespace Invader::Parser {
         this->attachment_5 = static_cast<HEK::FunctionOut>(this->attachment_5 + 1);
     }
 
-    void Invader::Parser::PointPhysics::post_parse_cache_file_data(Invader::Tag const &tag, std::optional<HEK::Pointer> pointer) {
+    void Invader::Parser::PointPhysics::post_parse_cache_file_data(Invader::Tag const &, std::optional<HEK::Pointer>) {
         this->air_friction /= 10000.0f;
         this->water_friction /= 10000.0f;
     }
 
-    void Invader::Parser::Projectile::post_parse_cache_file_data(Invader::Tag const &tag, std::optional<HEK::Pointer> pointer) {
+    void Invader::Parser::Projectile::post_parse_cache_file_data(Invader::Tag const &, std::optional<HEK::Pointer>) {
         this->initial_velocity *= TICK_RATE;
         this->final_velocity *= TICK_RATE;
     }
 
-    void Invader::Parser::ScenarioCutsceneTitle::post_parse_cache_file_data(Invader::Tag const &tag, std::optional<HEK::Pointer> pointer) {
+    void Invader::Parser::ScenarioCutsceneTitle::post_parse_cache_file_data(Invader::Tag const &, std::optional<HEK::Pointer>) {
         this->fade_in_time *= TICK_RATE;
         this->fade_out_time *= TICK_RATE;
         this->up_time *= TICK_RATE;
