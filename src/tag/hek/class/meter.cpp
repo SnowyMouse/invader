c2cfa5f076c9e693abd13b1c92ea0460f031588e
Kavawuvi <kavawuvi@tutanota.com>
2019-11-22 21:34:45 -0600

    Null out meter references

--- src/tag/hek/class/meter.cpp
+++ src/tag/hek/class/meter.cpp
@@ -6,8 +6,11 @@
 namespace Invader::HEK {
     void compile_meter_tag(CompiledTag &compiled, const std::byte *data, std::size_t size) {
         BEGIN_COMPILE(Meter)
-        ADD_DEPENDENCY_ADJUST_SIZES(tag.stencil_bitmaps);
-        ADD_DEPENDENCY_ADJUST_SIZES(tag.source_bitmap);
+        // These don't work in Halo anyway
+        //ADD_DEPENDENCY_ADJUST_SIZES(tag.stencil_bitmaps);
+        //ADD_DEPENDENCY_ADJUST_SIZES(tag.source_bitmap);
+        tag.stencil_bitmaps.tag_id = TagID::null_tag_id();
+        tag.source_bitmap.tag_id = TagID::null_tag_id();
         ADD_POINTER_FROM_INT32(tag.encoded_stencil.pointer, compiled.data.size());
         std::size_t data_size = tag.encoded_stencil.size;
         compiled.data.insert(compiled.data.end(), data, data + data_size);
