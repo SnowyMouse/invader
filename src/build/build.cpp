dabca1eddd9bf42bbc11ec2da409dae2dc7de246
Kavawuvi <kavawuvi@tutanota.com>
2019-11-28 09:52:17 -0600

    Fix endianness

--- src/build/build.cpp
+++ src/build/build.cpp
@@ -128,10 +128,10 @@ int main(int argc, const char **argv) {
     });
 
     if(build_options.forged_crc && ON_COLOR_TERM) {
-        if(std::strcmp(build_options.forged_crc, "56617021") == 0) {
+        if(std::strcmp(build_options.forged_crc, "21706156") == 0) {
             std::fprintf(stdout, "\x1B[38;5;51m");
         }
-        else if(std::strcmp(build_options.forged_crc, "43687521") == 0) {
+        else if(std::strcmp(build_options.forged_crc, "21756843") == 0) {
             std::fprintf(stdout, "\x1B[38;5;204m");
         }
     }
