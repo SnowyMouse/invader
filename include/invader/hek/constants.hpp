2ba44e4632f89dbab8aa92719ab79ef41baceafd
Kavawuvi <kavawuvi@tutanota.com>
2019-11-29 09:32:00 -0600

    Update constants

--- include/invader/hek/constants.hpp
+++ include/invader/hek/constants.hpp
@@ -3,12 +3,32 @@
 #ifndef INVADER__HEK__CONSTANTS_HPP
 #define INVADER__HEK__CONSTANTS_HPP
 
-#define TICK_RATE (30.0F)
-#define GRAVITY (3.2086612656F / TICK_RATE / TICK_RATE)
+/**
+ * Number of times the game performs most of its logic (physics, etc.)
+ */
+#define TICK_RATE 30.0F
+
+/**
+ * Number of world units per meter (1 world unit = 10 feet = 3.048 meters)
+ */
+#define WORLD_UNITS_PER_METER 3.048F
+
+/**
+ * Halo's gravity is equal to 9.78 meters per second squared, or approximately
+ * 0.997 g (0.997 times Earth's gravity).
+ */
+#define GRAVITY ((9.78F / WORLD_UNITS_PER_METER) / TICK_RATE / TICK_RATE)
+
+/**
+ * From Guerilla's self-documentation, measured in grams per milliliter
+ */
 #define AIR_DENSITY 0.0011F
 #define WATER_DENSITY 1.0F
 
-// I know HALO_PI is an option, but this is the constant Halo uses
-#define HALO_PI 3.1415927F
+/**
+ * I know M_PI is an option, but from looking at tool's output, this is
+ * approximately the constant Halo uses
+ */
+#define HALO_PI 3.14159274101257F
 
 #endif
