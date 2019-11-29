3f7a5c2c9369f0cff5a6c8020dd8f584f8499a38
Kavawuvi <kavawuvi@tutanota.com>
2019-11-29 14:11:21 -0600

    Antenna

--- src/invader.cmake
+++ src/invader.cmake
@@ -112,6 +112,7 @@ set(INVADER_SOURCE_FILES
     src/tag/parser/compile/font.cpp
     src/tag/parser/compile/particle.cpp
     src/tag/parser/compile/point_physics.cpp
+    src/tag/parser/compile/antenna.cpp
     src/bitmap/stb/stb_impl.c
 
     src/crc/crc32.c
