744494ea6fa50779a40eacc02c5daef40d188175
Kavawuvi <kavawuvi@tutanota.com>
2019-11-30 12:56:36 -0600

    Add a warning

--- src/tag/parser/compile/scenario.cpp
+++ src/tag/parser/compile/scenario.cpp
@@ -8,9 +8,33 @@ namespace Invader::Parser {
         if(!workload.cache_file_type.has_value()) {
             workload.cache_file_type = this->type;
         }
+        else {
+            workload.report_error(BuildWorkload2::ErrorType::ERROR_TYPE_FATAL_ERROR, "Multiple scenario tags are used", tag_index);
+            throw InvalidTagDataException();
+        }
+
+        if(this->scripts.size() > 0) {
+            if(this->source_files.size() == 0) {
+                eprintf_warn("Scenario tag has script data but no source file data");
+                eprintf_warn("This is DEPRECATED and will not be allowed in some future version of Invader");
+                workload.report_error(BuildWorkload2::ErrorType::ERROR_TYPE_WARNING, "To fix this, recompile the scripts", tag_index);
+            }
+            else {
+                // TODO: Recompile scripts
+                workload.report_error(BuildWorkload2::ErrorType::ERROR_TYPE_WARNING, "TODO: Implement script re-compiling", tag_index);
+            }
+        }
 
         // TODO: Swap endianness for script node table, adding a script syntax data and script string data block if none is present
         std::terminate();
+    }
+
+    void Scenario::post_compile(BuildWorkload2 &workload, std::size_t tag_index, std::size_t, std::size_t) {
+        // TODO: Position encounters and command lists
+        if(this->encounters.size() == 0 || this->command_lists.size() == 0) {
+            workload.report_error(BuildWorkload2::ErrorType::ERROR_TYPE_FATAL_ERROR, "TODO: Implement encounter and command list BSP location", tag_index);
+            std::terminate();
+        }
     }
 
     void ScenarioCutsceneTitle::pre_compile(BuildWorkload2 &, std::size_t, std::size_t, std::size_t) {
