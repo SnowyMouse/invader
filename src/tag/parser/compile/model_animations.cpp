327de0e39186a5b9c73799d6d260edfcfa08fdd2
Kavawuvi <kavawuvi@tutanota.com>
2019-12-02 13:34:44 -0600

    Do some checks

--- src/tag/parser/compile/model_animations.cpp
+++ src/tag/parser/compile/model_animations.cpp
@@ -1,12 +1,45 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <invader/tag/parser/parser.hpp>
+#include <invader/build/build_workload.hpp>
 
 namespace Invader::Parser {
     void ModelAnimations::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         std::size_t animation_count = this->animations.size();
+        std::size_t sound_count = this->sound_references.size();
         for(std::size_t i = 0; i < animation_count; i++) {
             auto *animation = this->animations.data() + i;
+
+            // Check to make sure indices aren't broken
+            std::size_t sound = animation->sound;
+            std::size_t sound_frame_index = animation->sound_frame_index;
+            std::size_t frame_count = animation->frame_count;
+            if(sound != NULL_INDEX) {
+                if(sound >= sound_count) {
+                    REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Animation #%zu has an invalid sound index (%zu >= %zu)", i, sound, sound_count);
+                }
+            }
+            else if(animation->sound_frame_index != 0) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING, tag_index, "Animation #%zu has a sound frame index set, but no sound is referenced", i);
+            }
+            if(sound_frame_index >= animation->frame_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Animation #%zu has an invalid sound frame index (%zu >= %zu)", i, sound_frame_index, frame_count);
+            }
+
+            std::size_t loop_frame_index = animation->loop_frame_index;
+            if(loop_frame_index >= animation->frame_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Animation #%zu has an invalid loop frame index (%zu >= %zu)", i, loop_frame_index, frame_count);
+            }
+
+            std::size_t key_frame_index = animation->key_frame_index;
+            if(key_frame_index >= animation->frame_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Animation #%zu has an invalid key frame index (%zu >= %zu)", i, key_frame_index, frame_count);
+            }
+
+            std::size_t second_key_frame_index = animation->second_key_frame_index;
+            if(second_key_frame_index >= animation->frame_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Animation #%zu has an invalid second key frame index (%zu >= %zu)", i, second_key_frame_index, frame_count);
+            }
 
             // Check if we already did things to this
             if(animation->main_animation_index) {
@@ -28,12 +61,13 @@ namespace Invader::Parser {
                 total_weight += animation->weight > 0.0F ? animation->weight : 1.0F;
 
                 // Get the next animation if there is one
-                auto next_animation = static_cast<std::uint16_t>(animation->next_animation);
-                if(next_animation == 0xFFFF) {
+                std::size_t next_animation = animation->next_animation;
+                if(next_animation == NULL_INDEX) {
                     break;
                 }
                 else if(next_animation >= animation_count) {
-                    throw OutOfBoundsException();
+                    REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Animation #%zu has an invalid next animation index (%zu >= %zu)", animation - this->animations.data(), next_animation, animation_count);
+                    throw InvalidTagDataException();
                 }
                 else {
                     animation = this->animations.data() + next_animation;
@@ -53,7 +87,7 @@ namespace Invader::Parser {
 
                     // Get the next animation if there is one
                     auto next_animation = static_cast<std::uint16_t>(animation->next_animation);
-                    if(next_animation == 0xFFFF) {
+                    if(next_animation == NULL_INDEX) {
                         break;
                     }
                     else {
@@ -63,5 +97,9 @@ namespace Invader::Parser {
 
             }
         }
+    }
+
+    void ModelAnimationsAnimation::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
+
     }
 }
