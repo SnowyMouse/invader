4a7cec470c5406a53d70e00dd259fd8b6d59cb6c
Kavawuvi <kavawuvi@tutanota.com>
2019-12-02 13:14:23 -0600

    Partially do model animations

--- src/invader.cmake
+++ src/invader.cmake
@@ -41,6 +41,7 @@ set(INVADER_SOURCE_FILES
     src/tag/parser/compile/item_collection.cpp
     src/tag/parser/compile/lens_flare.cpp
     src/tag/parser/compile/light.cpp
+    src/tag/parser/compile/model_animations.cpp
     src/tag/parser/compile/model_collision_geometry.cpp
     src/tag/parser/compile/object.cpp
     src/tag/parser/compile/particle.cpp
--- src/tag/parser/compile/model_animations.cpp
+++ src/tag/parser/compile/model_animations.cpp
@@ -3,11 +3,65 @@
 #include <invader/tag/parser/parser.hpp>
 
 namespace Invader::Parser {
-    void ModelAnimations::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
+    void ModelAnimations::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
+        std::size_t animation_count = this->animations.size();
+        for(std::size_t i = 0; i < animation_count; i++) {
+            auto *animation = this->animations.data() + i;
 
-    }
+            // Check if we already did things to this
+            if(animation->main_animation_index) {
+                continue;
+            }
 
-    void ModelAnimationAnimation::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
-        
+            bool multiple_animations = false;
+            float total_weight = 0.0F;
+
+            // Go through each animation. Make sure the weights are all correct.
+            while(true) {
+                // Set the main animation index
+                animation->main_animation_index = static_cast<std::uint32_t>(i);
+
+                // Set weight to a default value
+                animation->relative_weight = 1.0F;
+
+                // Increment total weight
+                total_weight += animation->weight > 0.0F ? animation->weight : 1.0F;
+
+                // Get the next animation if there is one
+                auto next_animation = static_cast<std::uint16_t>(animation->next_animation);
+                if(next_animation == 0xFFFF) {
+                    break;
+                }
+                else if(next_animation >= animation_count) {
+                    throw OutOfBoundsException();
+                }
+                else {
+                    animation = this->animations.data() + next_animation;
+                    multiple_animations = true;
+                }
+            }
+
+            // We will need to go down the rabbit hole here if we have multiple animations
+            if(multiple_animations) {
+                animation = this->animations.data() + i;
+                float total_weight_second_pass = 0.0F;
+
+                while(animation != nullptr) {
+                    // Set the weight
+                    total_weight_second_pass += animation->weight > 0.0F ? animation->weight : 1.0F;
+                    animation->relative_weight = total_weight_second_pass / total_weight;
+
+                    // Get the next animation if there is one
+                    auto next_animation = static_cast<std::uint16_t>(animation->next_animation);
+                    if(next_animation == 0xFFFF) {
+                        break;
+                    }
+                    else {
+                        animation = this->animations.data() + next_animation;
+                    }
+                }
+
+            }
+        }
     }
 }
