79181664cf7ba8a57ef1e5929e99e6a5225af39d
Kavawuvi <kavawuvi@tutanota.com>
2019-12-04 09:17:48 -0600

    Add predicted resources

--- src/tag/parser/compile/object.cpp
+++ src/tag/parser/compile/object.cpp
@@ -20,39 +20,6 @@ namespace Invader::Parser {
         this->sine_uphill_falloff_angle = static_cast<float>(std::sin(this->uphill_falloff_angle));
         this->sine_uphill_cutoff_angle = static_cast<float>(std::sin(this->uphill_cutoff_angle));
     }
-    void Biped::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t offset) {
-        auto &head_index = reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset)->head_model_node_index;
-        auto &model_id = this->model.tag_id;
-
-        this->head_model_node_index = NULL_INDEX;
-        if(model_id.is_null()) {
-            workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped is missing a model tag, so it will not spawn", tag_index);
-        }
-        else {
-            auto &model_tag = workload.tags[model_id.index];
-            auto &model_tag_header = workload.structs[*model_tag.base_struct];
-            auto &model_tag_header_struct = *reinterpret_cast<GBXModel::struct_little *>(model_tag_header.data.data());
-            std::size_t node_count = model_tag_header_struct.nodes.count.read();
-            if(node_count) {
-                auto *nodes = reinterpret_cast<GBXModelNode::struct_little *>(workload.structs[*model_tag_header.resolve_pointer(&model_tag_header_struct.nodes.pointer)].data.data());
-                for(std::size_t n = 0; n < node_count; n++) {
-                    auto &node = nodes[n];
-                    if(std::strncmp(node.name.string, "bip01 head", sizeof(node.name.string) - 1) == 0) {
-                        this->head_model_node_index = static_cast<HEK::Index>(n);
-                    }
-                }
-
-                if(this->head_model_node_index == NULL_INDEX) {
-                    workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped model has no \"bip01 head\" node, so the biped will not spawn", tag_index);
-                }
-            }
-            else {
-                workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped model has no nodes, so the biped will not spawn", tag_index);
-            }
-        }
-
-        head_index = this->head_model_node_index;
-    }
     void Vehicle::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
         this->object_type = HEK::ObjectType::OBJECT_TYPE_VEHICLE;
         compile_object(*this);
@@ -146,5 +113,149 @@ namespace Invader::Parser {
                 this->error_angle.to = DEGREES_TO_RADIANS(2.5F);
             }
         }
+    }
+
+    static void recursively_get_all_predicted_resources_from_struct(const BuildWorkload &workload, std::size_t struct_index, std::vector<std::size_t> &resources, bool ignore_shader_resources) {
+        auto &s = workload.structs[struct_index];
+        for(auto &d : s.dependencies) {
+            std::size_t tag_index = d.tag_index;
+            auto &dt = workload.tags[tag_index];
+            switch(dt.tag_class_int) {
+                case TagClassInt::TAG_CLASS_BITMAP:
+                case TagClassInt::TAG_CLASS_SOUND:
+                    resources.push_back(tag_index);
+                    break;
+                case TagClassInt::TAG_CLASS_SHADER_ENVIRONMENT:
+                case TagClassInt::TAG_CLASS_SHADER_MODEL:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_CHICAGO:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_CHICAGO_EXTENDED:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_GENERIC:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_GLASS:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_METER:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_PLASMA:
+                case TagClassInt::TAG_CLASS_SHADER_TRANSPARENT_WATER:
+                    if(ignore_shader_resources) {
+                        break;
+                    }
+                    // fallthrough
+                case TagClassInt::TAG_CLASS_GBXMODEL:
+                    recursively_get_all_predicted_resources_from_struct(workload, *dt.base_struct, resources, false);
+                    break;
+                default:
+                    continue;
+            }
+        }
+        for(auto &p : s.pointers) {
+            recursively_get_all_predicted_resources_from_struct(workload, p.struct_index, resources, false);
+        }
+    }
+
+    // Go through each tag this depends on that can have predicted resources. This is to preload assets when the object spawns to reduce hitching. Using maps in RAM makes this basically pointless, though.
+    static void calculate_object_predicted_resources(BuildWorkload &workload, std::size_t struct_index) {
+        std::vector<std::size_t> resources;
+        recursively_get_all_predicted_resources_from_struct(workload, struct_index, resources, true);
+        auto &s = workload.structs[struct_index];
+
+        for(std::size_t a = 0; a < resources.size(); a++) {
+            for(std::size_t b = a + 1; b < resources.size(); b++) {
+                if(resources[a] == resources[b]) {
+                    resources.erase(resources.begin() + b);
+                    b--;
+                }
+            }
+        }
+
+        std::size_t resources_count = resources.size();
+        if(resources_count > 0) {
+            auto &predicted_resource_pointer = s.pointers.emplace_back();
+            predicted_resource_pointer.struct_index = workload.structs.size();
+
+            auto &object = *reinterpret_cast<Parser::Object::struct_little *>(s.data.data());
+            predicted_resource_pointer.offset = reinterpret_cast<const std::byte *>(&object.predicted_resources.pointer) - reinterpret_cast<const std::byte *>(&object);
+            object.predicted_resources.count = static_cast<std::uint32_t>(resources_count);
+            std::vector<HEK::PredictedResource<HEK::LittleEndian>> predicted_resources;
+
+            auto &prs = workload.structs.emplace_back();
+            predicted_resources.reserve(resources_count);
+            for(std::size_t r : resources) {
+                auto &resource = predicted_resources.emplace_back();
+                auto &resource_tag = workload.tags[r];
+                resource.type = resource_tag.tag_class_int == TagClassInt::TAG_CLASS_BITMAP ? HEK::PredictedResourceType::PREDICTED_RESOUCE_TYPE_BITMAP : HEK::PredictedResourceType::PREDICTED_RESOUCE_TYPE_SOUND;
+                resource.tag = HEK::TagID { static_cast<std::uint32_t>(r) };
+                auto &resource_dep = prs.dependencies.emplace_back();
+                resource_dep.tag_id_only = true;
+                resource_dep.tag_index = r;
+                resource_dep.offset = reinterpret_cast<const std::byte *>(&resource.tag) - reinterpret_cast<const std::byte *>(predicted_resources.data());
+            }
+            prs.data.insert(prs.data.begin(), reinterpret_cast<const std::byte *>(predicted_resources.data()), reinterpret_cast<const std::byte *>(predicted_resources.data() + resources_count));
+        }
+    }
+
+    void Biped::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t offset) {
+        auto &head_index = reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset)->head_model_node_index;
+        auto &model_id = this->model.tag_id;
+
+        this->head_model_node_index = NULL_INDEX;
+        if(model_id.is_null()) {
+            workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped is missing a model tag, so it will not spawn", tag_index);
+        }
+        else {
+            auto &model_tag = workload.tags[model_id.index];
+            auto &model_tag_header = workload.structs[*model_tag.base_struct];
+            auto &model_tag_header_struct = *reinterpret_cast<GBXModel::struct_little *>(model_tag_header.data.data());
+            std::size_t node_count = model_tag_header_struct.nodes.count.read();
+            if(node_count) {
+                auto *nodes = reinterpret_cast<GBXModelNode::struct_little *>(workload.structs[*model_tag_header.resolve_pointer(&model_tag_header_struct.nodes.pointer)].data.data());
+                for(std::size_t n = 0; n < node_count; n++) {
+                    auto &node = nodes[n];
+                    if(std::strncmp(node.name.string, "bip01 head", sizeof(node.name.string) - 1) == 0) {
+                        this->head_model_node_index = static_cast<HEK::Index>(n);
+                    }
+                }
+
+                if(this->head_model_node_index == NULL_INDEX) {
+                    workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped model has no \"bip01 head\" node, so the biped will not spawn", tag_index);
+                }
+            }
+            else {
+                workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_WARNING, "Biped model has no nodes, so the biped will not spawn", tag_index);
+            }
+        }
+
+        head_index = this->head_model_node_index;
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Vehicle::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Weapon::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Equipment::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Garbage::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Projectile::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Scenery::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void Placeholder::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void SoundScenery::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void DeviceMachine::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void DeviceControl::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
+    }
+    void DeviceLightFixture::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t) {
+        calculate_object_predicted_resources(workload, struct_index);
     }
 }
