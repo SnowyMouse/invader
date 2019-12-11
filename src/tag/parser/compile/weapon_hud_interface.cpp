fa39239b57b44a50f96cdc858a41036b727d7821
Kavawuvi <kavawuvi@tutanota.com>
2019-12-11 09:54:03 -0600

    Turn it into a bitmask

--- src/tag/parser/compile/weapon_hud_interface.cpp
+++ src/tag/parser/compile/weapon_hud_interface.cpp
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <invader/tag/parser/parser.hpp>
+#include <invader/build/build_workload.hpp>
 
 namespace Invader::Parser {
-    void WeaponHUDInterface::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
+    void WeaponHUDInterface::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         std::uint32_t crosshair_types = 0;
         for(auto &c : this->crosshairs) {
             crosshair_types |= (1 << c.crosshair_type);
         }
-        this->crosshair_types = crosshair_types;
+        this->crosshair_types = *reinterpret_cast<HEK::WeaponHUDInterfaceCrosshairTypeFlags *>(crosshair_types);
+
+        if(workload.engine_target != HEK::CacheFileEngine::CACHE_FILE_DARK_CIRCLET && this->crosshair_types.zoom == 0) {
+            std::size_t zooms = 0;
+            for(auto &c : this->crosshairs) {
+                for(auto &o : c.crosshair_overlays) {
+                    if(o.flags.dont_show_when_zoomed || o.flags.show_only_when_zoomed) {
+                        zooms++;
+                    }
+                }
+            }
+            if(zooms) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING, tag_index, "%zu overlay%s set to change on zoom, but no zoom crosshairs exist.", zooms, zooms == 1 ? " is" : "s are");
+            }
+        }
     }
 }
