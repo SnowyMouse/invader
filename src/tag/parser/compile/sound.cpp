cffc9819a2765b33ba356768bdc0db29b4a35a7e
Kavawuvi <kavawuvi@tutanota.com>
2020-01-06 23:14:10 -0600

    Check for infinite loops and out-of-bounds permutations

--- src/tag/parser/compile/sound.cpp
+++ src/tag/parser/compile/sound.cpp
@@ -62,10 +62,62 @@ namespace Invader::Parser {
         this->samples_pointer = 0xFFFFFFFF;
     }
 
-    void SoundPitchRange::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
+    void SoundPitchRange::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t struct_offset) {
         this->unknown_ffffffff_0 = 0xFFFFFFFF;
         this->unknown_ffffffff_1 = 0xFFFFFFFF;
         this->playback_rate = 1.0f / (this->natural_pitch == 0.0F ? 1.0F : this->natural_pitch);
+
+        // Make sure all of the permutations are valid
+        std::size_t permutation_count = this->permutations.size();
+        std::size_t actual_permutation_count = this->actual_permutation_count;
+        if(actual_permutation_count > permutation_count) {
+            REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Actual permutation count in pitch range #%zu exceeds the number of permutations (%zu >= %zu)", struct_offset / sizeof(struct_little), actual_permutation_count, permutation_count);
+            throw InvalidTagDataException();
+        }
+
+        // Now check errors and unused permutations
+        std::vector<bool> referenced(permutation_count, false);
+        std::fill(referenced.begin(), referenced.begin() + actual_permutation_count, true);
+        for(std::size_t i = 0; i < permutation_count; i++) {
+            auto &permutation = this->permutations[i];
+            std::size_t next_permutation = permutation.next_permutation_index;
+            if(next_permutation == NULL_INDEX) {
+                continue;
+            }
+            else if(next_permutation >= permutation_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Permutation #%zu in pitch range #%zu has an invalid next index (%zu >= %zu)", i, struct_offset / sizeof(struct_little), next_permutation, permutation_count);
+                throw InvalidTagDataException();
+            }
+            else {
+                referenced[next_permutation] = true;
+            }
+        }
+
+        // List unused permutations
+        for(std::size_t i = 0; i < permutation_count; i++) {
+            if(!referenced[i]) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING_PEDANTIC, tag_index, "Permutation #%zu in pitch range #%zu is unused", i, struct_offset / sizeof(struct_little));
+            }
+        }
+
+        // Look for endless loops
+        for(std::size_t a = 0; a < actual_permutation_count; a++) {
+            // Reset referenced to all false
+            std::fill(referenced.begin(), referenced.begin() + permutation_count, false);
+
+            // Start at where we are
+            std::size_t permutation_index = a;
+
+            // Keep going until we hit a null index or we loop forever
+            while(permutation_index != NULL_INDEX) {
+                if(referenced[permutation_index]) {
+                    REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Actual permutation #%zu (%s) in pitch range #%zu will never end", a, this->permutations[a].name.string, struct_offset / sizeof(struct_little));
+                    break;
+                }
+                referenced[permutation_index] = true;
+                permutation_index = this->permutations[permutation_index].next_permutation_index;
+            }
+        }
     }
 
     void Sound::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
@@ -88,10 +140,20 @@ namespace Invader::Parser {
         if(this->channel_count == HEK::SoundChannelCount::SOUND_CHANNEL_COUNT_MONO && this->sample_rate == HEK::SoundSampleRate::SOUND_SAMPLE_RATE_44100_HZ && workload.engine_target != HEK::CacheFileEngine::CACHE_FILE_DARK_CIRCLET) {
             REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING, tag_index, "Sound is 44.1 kHz AND mono. The target engine will not play this.");
         }
+
+        // If we didn't split long sounds into permutations, go through each permutation and Jason Jones it
+        if(!this->flags.split_long_sound_into_permutations) {
+            for(auto &pr : this->pitch_ranges) {
+                pr.actual_permutation_count = static_cast<std::uint16_t>(pr.permutations.size());
+                for(auto &p : pr.permutations) {
+                    p.next_permutation_index = NULL_INDEX;
+                }
+            }
+        }
     }
 
     void SoundLooping::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
-        this->unknown_int = 0xFFFFFFFF;
-        this->unknown_float = 15.0F;
+        this->unknown_int = 0xFFFFFFFF; // this is probably a pointer, but we should check to make sure it isn't something important
+        this->unknown_float = 15.0F; // TODO: Figure this out
     }
 }
