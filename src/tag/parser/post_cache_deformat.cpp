5b6a44601976be47f73e0177d6b86b03f48e4041
Kavawuvi <kavawuvi@tutanota.com>
2020-02-21 20:49:26 -0600

    Comment this out for now

--- src/tag/parser/post_cache_deformat.cpp
+++ src/tag/parser/post_cache_deformat.cpp
@@ -40,7 +40,7 @@ namespace Invader::Parser {
         }
 
         // Put scripts in if need be
-        this->source_files.clear();
+        /*this->source_files.clear();
         try {
             if(this->scripts.size() != 0) {
                 auto script = Tokenizer::detokenize(ScriptTree::decompile_script_tree(Compiler::decompile_scenario(*this)));
@@ -48,13 +48,11 @@ namespace Invader::Parser {
                 auto &source_file = this->source_files.emplace_back();
                 std::snprintf(source_file.name.string, sizeof(source_file.name.string), "extracted");
                 source_file.source = std::vector<std::byte>(script_data, script_data + script.size());
-
-                this->source_files.clear(); // clear this anyway
             }
         }
         catch(std::exception &e) {
             eprintf_error("Failed to decompile scripts; scenario will not have any source data: %s", e.what());
-        }
+        }*/
 
         // And lastly, for consistency sake, remove all tag IDs and zero out the pointer
         this->postprocess_hek_data();
