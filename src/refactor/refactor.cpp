f470b3a1fa11c5acaf62413205cb74fcf20c64be
Kavawuvi <kavawuvi@tutanota.com>
2020-02-28 04:05:02 -0600

    Slim down this code

--- src/refactor/refactor.cpp
+++ src/refactor/refactor.cpp
@@ -29,109 +29,15 @@ std::size_t refactor_tags(const char *file_path, std::vector<std::pair<TagFilePa
         const auto *header = reinterpret_cast<const Invader::HEK::TagFileHeader *>(tag->data());
         Invader::HEK::TagFileHeader::validate_header(header, tag->size());
 
-        #define REFACTOR_TAG_CLASS(class_struct, tag_class_int) case Invader::TagClassInt::tag_class_int: { \
-            auto tag_data = Invader::Parser::class_struct::parse_hek_tag_file(tag->data(), tag->size()); \
-            for (auto &r : replacements) { \
-                count += tag_data.refactor_reference(r.first.path.c_str(), r.first.class_int, r.second.path.c_str(), r.second.class_int); \
-            } \
-            if(count) { \
-                file_data = tag_data.generate_hek_tag_data(Invader::TagClassInt::tag_class_int); \
-            } \
-            else { \
-                return count; \
-            } \
-            break; \
+        auto tag_data = Invader::Parser::ParserStruct::parse_hek_tag_file(tag->data(), tag->size());
+        for (auto &r : replacements) {
+            count += tag_data->refactor_reference(r.first.path.c_str(), r.first.class_int, r.second.path.c_str(), r.second.class_int);
         }
-
-        switch(header->tag_class_int) {
-            REFACTOR_TAG_CLASS(Actor, TAG_CLASS_ACTOR)
-            REFACTOR_TAG_CLASS(ActorVariant, TAG_CLASS_ACTOR_VARIANT)
-            REFACTOR_TAG_CLASS(Antenna, TAG_CLASS_ANTENNA)
-            REFACTOR_TAG_CLASS(ModelAnimations, TAG_CLASS_MODEL_ANIMATIONS)
-            REFACTOR_TAG_CLASS(Biped, TAG_CLASS_BIPED)
-            REFACTOR_TAG_CLASS(Bitmap, TAG_CLASS_BITMAP)
-            REFACTOR_TAG_CLASS(ModelCollisionGeometry, TAG_CLASS_MODEL_COLLISION_GEOMETRY)
-            REFACTOR_TAG_CLASS(ColorTable, TAG_CLASS_COLOR_TABLE)
-            REFACTOR_TAG_CLASS(Contrail, TAG_CLASS_CONTRAIL)
-            REFACTOR_TAG_CLASS(DeviceControl, TAG_CLASS_DEVICE_CONTROL)
-            REFACTOR_TAG_CLASS(Decal, TAG_CLASS_DECAL)
-            REFACTOR_TAG_CLASS(UIWidgetDefinition, TAG_CLASS_UI_WIDGET_DEFINITION)
-            REFACTOR_TAG_CLASS(InputDeviceDefaults, TAG_CLASS_INPUT_DEVICE_DEFAULTS)
-            REFACTOR_TAG_CLASS(Device, TAG_CLASS_DEVICE)
-            REFACTOR_TAG_CLASS(DetailObjectCollection, TAG_CLASS_DETAIL_OBJECT_COLLECTION)
-            REFACTOR_TAG_CLASS(Effect, TAG_CLASS_EFFECT)
-            REFACTOR_TAG_CLASS(Equipment, TAG_CLASS_EQUIPMENT)
-            REFACTOR_TAG_CLASS(Flag, TAG_CLASS_FLAG)
-            REFACTOR_TAG_CLASS(Fog, TAG_CLASS_FOG)
-            REFACTOR_TAG_CLASS(Font, TAG_CLASS_FONT)
-            REFACTOR_TAG_CLASS(MaterialEffects, TAG_CLASS_MATERIAL_EFFECTS)
-            REFACTOR_TAG_CLASS(Garbage, TAG_CLASS_GARBAGE)
-            REFACTOR_TAG_CLASS(Glow, TAG_CLASS_GLOW)
-            REFACTOR_TAG_CLASS(GrenadeHUDInterface, TAG_CLASS_GRENADE_HUD_INTERFACE)
-            REFACTOR_TAG_CLASS(HUDMessageText, TAG_CLASS_HUD_MESSAGE_TEXT)
-            REFACTOR_TAG_CLASS(HUDNumber, TAG_CLASS_HUD_NUMBER)
-            REFACTOR_TAG_CLASS(HUDGlobals, TAG_CLASS_HUD_GLOBALS)
-            REFACTOR_TAG_CLASS(Item, TAG_CLASS_ITEM)
-            REFACTOR_TAG_CLASS(ItemCollection, TAG_CLASS_ITEM_COLLECTION)
-            REFACTOR_TAG_CLASS(DamageEffect, TAG_CLASS_DAMAGE_EFFECT)
-            REFACTOR_TAG_CLASS(LensFlare, TAG_CLASS_LENS_FLARE)
-            REFACTOR_TAG_CLASS(Lightning, TAG_CLASS_LIGHTNING)
-            REFACTOR_TAG_CLASS(DeviceLightFixture, TAG_CLASS_DEVICE_LIGHT_FIXTURE)
-            REFACTOR_TAG_CLASS(Light, TAG_CLASS_LIGHT)
-            REFACTOR_TAG_CLASS(SoundLooping, TAG_CLASS_SOUND_LOOPING)
-            REFACTOR_TAG_CLASS(DeviceMachine, TAG_CLASS_DEVICE_MACHINE)
-            REFACTOR_TAG_CLASS(Globals, TAG_CLASS_GLOBALS)
-            REFACTOR_TAG_CLASS(Meter, TAG_CLASS_METER)
-            REFACTOR_TAG_CLASS(LightVolume, TAG_CLASS_LIGHT_VOLUME)
-            REFACTOR_TAG_CLASS(GBXModel, TAG_CLASS_GBXMODEL)
-            REFACTOR_TAG_CLASS(MultiplayerScenarioDescription, TAG_CLASS_MULTIPLAYER_SCENARIO_DESCRIPTION)
-            REFACTOR_TAG_CLASS(Object, TAG_CLASS_OBJECT)
-            REFACTOR_TAG_CLASS(Particle, TAG_CLASS_PARTICLE)
-            REFACTOR_TAG_CLASS(ParticleSystem, TAG_CLASS_PARTICLE_SYSTEM)
-            REFACTOR_TAG_CLASS(Physics, TAG_CLASS_PHYSICS)
-            REFACTOR_TAG_CLASS(Placeholder, TAG_CLASS_PLACEHOLDER)
-            REFACTOR_TAG_CLASS(PointPhysics, TAG_CLASS_POINT_PHYSICS)
-            REFACTOR_TAG_CLASS(Projectile, TAG_CLASS_PROJECTILE)
-            REFACTOR_TAG_CLASS(WeatherParticleSystem, TAG_CLASS_WEATHER_PARTICLE_SYSTEM)
-            REFACTOR_TAG_CLASS(Scenery, TAG_CLASS_SCENERY)
-            REFACTOR_TAG_CLASS(ShaderTransparentChicagoExtended, TAG_CLASS_SHADER_TRANSPARENT_CHICAGO_EXTENDED)
-            REFACTOR_TAG_CLASS(ShaderTransparentChicago, TAG_CLASS_SHADER_TRANSPARENT_CHICAGO)
-            REFACTOR_TAG_CLASS(Scenario, TAG_CLASS_SCENARIO)
-            REFACTOR_TAG_CLASS(ShaderEnvironment, TAG_CLASS_SHADER_ENVIRONMENT)
-            REFACTOR_TAG_CLASS(ShaderTransparentGlass, TAG_CLASS_SHADER_TRANSPARENT_GLASS)
-            REFACTOR_TAG_CLASS(Shader, TAG_CLASS_SHADER)
-            REFACTOR_TAG_CLASS(Sky, TAG_CLASS_SKY)
-            REFACTOR_TAG_CLASS(ShaderTransparentMeter, TAG_CLASS_SHADER_TRANSPARENT_METER)
-            REFACTOR_TAG_CLASS(Sound, TAG_CLASS_SOUND)
-            REFACTOR_TAG_CLASS(SoundEnvironment, TAG_CLASS_SOUND_ENVIRONMENT)
-            REFACTOR_TAG_CLASS(ShaderModel, TAG_CLASS_SHADER_MODEL)
-            REFACTOR_TAG_CLASS(ShaderTransparentGeneric, TAG_CLASS_SHADER_TRANSPARENT_GENERIC)
-            REFACTOR_TAG_CLASS(TagCollection, TAG_CLASS_UI_WIDGET_COLLECTION)
-            REFACTOR_TAG_CLASS(ShaderTransparentPlasma, TAG_CLASS_SHADER_TRANSPARENT_PLASMA)
-            REFACTOR_TAG_CLASS(SoundScenery, TAG_CLASS_SOUND_SCENERY)
-            REFACTOR_TAG_CLASS(StringList, TAG_CLASS_STRING_LIST)
-            REFACTOR_TAG_CLASS(ShaderTransparentWater, TAG_CLASS_SHADER_TRANSPARENT_WATER)
-            REFACTOR_TAG_CLASS(TagCollection, TAG_CLASS_TAG_COLLECTION)
-            REFACTOR_TAG_CLASS(CameraTrack, TAG_CLASS_CAMERA_TRACK)
-            REFACTOR_TAG_CLASS(Dialogue, TAG_CLASS_DIALOGUE)
-            REFACTOR_TAG_CLASS(UnitHUDInterface, TAG_CLASS_UNIT_HUD_INTERFACE)
-            REFACTOR_TAG_CLASS(Unit, TAG_CLASS_UNIT)
-            REFACTOR_TAG_CLASS(UnicodeStringList, TAG_CLASS_UNICODE_STRING_LIST)
-            REFACTOR_TAG_CLASS(VirtualKeyboard, TAG_CLASS_VIRTUAL_KEYBOARD)
-            REFACTOR_TAG_CLASS(Vehicle, TAG_CLASS_VEHICLE)
-            REFACTOR_TAG_CLASS(Weapon, TAG_CLASS_WEAPON)
-            REFACTOR_TAG_CLASS(Wind, TAG_CLASS_WIND)
-            REFACTOR_TAG_CLASS(WeaponHUDInterface, TAG_CLASS_WEAPON_HUD_INTERFACE)
-            REFACTOR_TAG_CLASS(ScenarioStructureBSP, TAG_CLASS_SCENARIO_STRUCTURE_BSP)
-
-            case Invader::HEK::TagClassInt::TAG_CLASS_PREFERENCES_NETWORK_GAME:
-            case Invader::HEK::TagClassInt::TAG_CLASS_SPHEROID:
-            case Invader::HEK::TagClassInt::TAG_CLASS_CONTINUOUS_DAMAGE_EFFECT:
-            case Invader::HEK::TagClassInt::TAG_CLASS_MODEL:
-            case Invader::HEK::TagClassInt::TAG_CLASS_NONE:
-            case Invader::HEK::TagClassInt::TAG_CLASS_NULL:
-                eprintf_error("Error: Cannot handle %s.", tag_class_to_extension(header->tag_class_int));
-                return EXIT_FAILURE;
+        if(count) {
+            file_data = tag_data->generate_hek_tag_data(header->tag_class_int);
+        }
+        else {
+            return count;
         }
     }
     catch(std::exception &e) {
