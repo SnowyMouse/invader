246cee989113f6a1bdf4ecb027fdc74c8218b168
Kavawuvi <kavawuvi@tutanota.com>
2020-03-04 16:47:35 -0600

    Refine some of the checks here

--- src/edit/qt/tag_editor_array_widget.cpp
+++ src/edit/qt/tag_editor_array_widget.cpp
@@ -229,14 +229,16 @@ namespace Invader::EditQt {
         auto *value = this->get_struct_value();
         std::size_t count = value->get_array_size();
         int index = this->current_index();
+        auto index_unsigned = static_cast<std::size_t>(index);
+        bool selection = index >= 0;
 
         auto min = value->get_array_minimum_size();
         auto max = value->get_array_maximum_size();
 
-        this->delete_button->setEnabled(index >= min);
+        this->delete_button->setEnabled(selection && index_unsigned >= min);
         this->clear_button->setEnabled(count > 0 && min == 0);
-        this->shift_down_button->setEnabled(index > 0);
-        this->shift_up_button->setEnabled(index >= 0 ? static_cast<std::size_t>(index) + 1 < count : false);
+        this->shift_down_button->setEnabled(selection && index_unsigned > 0);
+        this->shift_up_button->setEnabled(selection && index_unsigned + 1 < count);
         this->add_button->setEnabled(count < max);
         this->duplicate_button->setEnabled(index >= 0 && this->add_button->isEnabled());
     }
