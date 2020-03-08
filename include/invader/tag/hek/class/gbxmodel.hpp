96b8244d6cfed21d36de5c8420bace41b51d36cc
Kavawuvi <kavawuvi@tutanota.com>
2020-03-08 13:03:13 -0500

    Add header guards to the juhbexmohdell

--- include/invader/tag/hek/class/gbxmodel.hpp
+++ include/invader/tag/hek/class/gbxmodel.hpp
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#ifndef INVADER__TAG__HEK__CLASS__GBXMODEL_HPP
+#define INVADER__TAG__HEK__CLASS__GBXMODEL_HPP
+
 namespace Invader::Parser {
     struct GBXModel;
 }
@@ -7,3 +10,5 @@ namespace Invader::Parser {
 namespace Invader::HEK {
     void uncache_model_markers(Parser::GBXModel &model);
 }
+
+#endif
