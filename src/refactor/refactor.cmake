d68e3be7dcefde2ddd6fbb6a60df4ac828972004
Kavawuvi <kavawuvi@tutanota.com>
2020-03-11 13:36:47 -0500

    Add INVADER_REFACTOR build option
    Fix that!

--- src/refactor/refactor.cmake
+++ src/refactor/refactor.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 if(NOT DEFINED ${INVADER_REFACTOR})
-    set(INVADER_STRIP true CACHE BOOL "Build invader-refactor (refactors tag paths)")
+    set(INVADER_REFACTOR true CACHE BOOL "Build invader-refactor (refactors tag paths)")
 endif()
 
 if(${INVADER_REFACTOR})
