235ee23fc4eeb26ee0de29efacb15a3635955614
Kavawuvi <kavawuvi@tutanota.com>
2020-03-12 16:51:29 -0500

    Center on open

--- src/edit/qt/editor/subwindow/tag_editor_subwindow.cpp
+++ src/edit/qt/editor/subwindow/tag_editor_subwindow.cpp
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <QGuiApplication>
+#include <QScreen>
+
 #include "tag_editor_subwindow.hpp"
 #include "../tag_editor_window.hpp"
 
 namespace Invader::EditQt {
     TagEditorSubwindow::TagEditorSubwindow(TagEditorWindow *parent_window) : parent_window(parent_window) {
         this->setWindowTitle(parent_window->get_file().tag_path.c_str());
+
+        // Center this
+        this->setGeometry(
+            QStyle::alignedRect(
+                Qt::LeftToRight,
+                Qt::AlignCenter,
+                this->size(),
+                QGuiApplication::primaryScreen()->geometry()
+            )
+        );
     }
 }
