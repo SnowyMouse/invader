95a3bd15e2313b83298ff3173698687a7569570c
Kavawuvi <kavawuvi@tutanota.com>
2020-04-03 13:05:07 -0500

    Add nightly link

--- src/version.cpp
+++ src/version.cpp
@@ -40,21 +40,24 @@ namespace Invader {
                "This program is licensed under the GNU General Public License v3.0.\n"
                "\n"
                "Credits:\n"
-               "  Kavawuvi                       - Lead Developer, Project owner\n"
-               "  Vaporeon                       - Testing & QA, AUR maintainer, Development\n"
+               "  Kavawuvi                     - Lead Developer, Project owner\n"
+               "  Vaporeon                     - Testing & QA, AUR maintainer, Development\n"
                "\n"
                "Special thanks:\n"
-               "  Tucker933                      - Hosting @ https://invader.opencarnage.net\n"
-               "  MosesofEgypt                   - Tag data & model data research and code,\n"
-               "                                   mouth data generation, ADPCM-XQ Xbox ADPCM\n"
-               "                                   conversion\n"
+               "  Tucker933                    - Hosting @ https://invader.opencarnage.net\n"
+               "  MosesofEgypt                 - Tag data & model data research and code,\n"
+               "                                 mouth data generation, ADPCM-XQ Xbox ADPCM\n"
+               "                                 conversion\n"
                "\n"
                "Software used in this program:\n"
-               "  FreeType                       - https://www.freetype.org/\n"
-               "  STB library                    - https://github.com/nothings/stb\n"
+               "  FreeType                     - https://www.freetype.org/\n"
+               "  STB library                  - https://github.com/nothings/stb\n"
                "\n"
-               "Other links:\n"
-               "  Invader source code            - https://github.com/Kavawuvi/invader\n"
-               "  Mo's Editing Kit               - https://github.com/MosesofEgypt/mek/\n";
+               "Links:\n"
+               "  Invader source code          - https://github.com/Kavawuvi/invader\n"
+               #ifdef SHOW_NIGHTLY_LINK
+               "  Invader nightly builds       - https://invader.opencarnage.net/builds/nightly/\n"
+               #endif
+               ;
     }
 }
