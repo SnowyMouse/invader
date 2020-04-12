6700f5c8a0c79ebe7415f2692aae0541aa65a02d
Kavawuvi <kavawuvi@tutanota.com>
2020-04-11 20:18:08 -0500

    Refactor font rendering

--- include/invader/tag/parser/compile/font.hpp
+++ include/invader/tag/parser/compile/font.hpp
@@ -11,6 +11,46 @@ namespace Invader::Parser {
      * @param font font tag to generate character tables
      */
     void generate_character_tables(Font &font);
+    
+    /**
+     * Draw text with the given font onto a bitmap (0xAARRGGBB)
+     * @param pixels    pointer to a buffer of pixels (must be at least width * height pixels)
+     * @param width     width of bitmap
+     * @param height    height of bitmap
+     * @param text      text to render
+     * @param font_data font to use
+     * @param color     color to use (0xAARRGGBB)
+     */
+    void draw_text(std::uint32_t *pixels, std::int32_t width, std::int32_t height, const char16_t *text, const Parser::Font &font_data, std::uint32_t color = 0xFFFFFFFF);
+    
+    /**
+     * Draw text with the given font onto a bitmap (0xAARRGGBB)
+     * @param pixels    pointer to a buffer of pixels (must be at least width * height pixels)
+     * @param width     width of bitmap
+     * @param height    height of bitmap
+     * @param text      text to render
+     * @param font_data font to use
+     * @param color     color to use (0xAARRGGBB)
+     */
+    void draw_text(std::uint32_t *pixels, std::int32_t width, std::int32_t height, const char *text, const Parser::Font &font_data, std::uint32_t color = 0xFFFFFFFF);
+    
+    /**
+     * Get the dimensions of a bitmap with the given text and font
+     * @param width     width reference to write to
+     * @param height    height reference to write to
+     * @param text      text to use
+     * @param font_data font to use
+     */
+    void get_dimensions(std::int32_t &width, std::int32_t &height, const char16_t *text, const Parser::Font &font_data);
+    
+    /**
+     * Get the dimensions of a bitmap with the given text and font
+     * @param width     width reference to write to
+     * @param height    height reference to write to
+     * @param text      text to use
+     * @param font_data font to use
+     */
+    void get_dimensions(std::int32_t &width, std::int32_t &height, const char *text, const Parser::Font &font_data);
 }
 
 #endif
