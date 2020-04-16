bf00648974ffa11e40a793e03c7db4932afeeb5b
Kavawuvi <kavawuvi@tutanota.com>
2020-04-16 03:05:00 -0500

    Finish refactoring error handling

--- include/invader/extract/extraction.hpp
+++ include/invader/extract/extraction.hpp
@@ -16,12 +16,51 @@ namespace Invader {
     public:
         /**
          * Extract a single tag from a map
-         * @param tag tag from a loaded map to extract
-         * @return    extracted tag
+         * @param tag             tag from a loaded map to extract
+         * @param reporting_level reporting level to use
+         * @return                extracted tag
          */
-        static std::vector<std::byte> extract_tag(const Tag &tag);
+        static std::vector<std::byte> extract_single_tag(const Tag &tag, ReportingLevel reporting_level = ReportingLevel::REPORTING_LEVEL_ALL);
+        
+        /**
+         * @param map             map to read
+         * @param tags            tag directory to extract to
+         * @param queries         search queries to look for
+         * @param recursive       also extract tags depended by a tag
+         * @param overwrite       overwrite tag files that exist
+         * @param non_mp_globals  allow extraction of non-multiplayer globals
+         * @param reporting_level reporting level to use
+         */
+        static void extract_map(const Map &map, const std::string &tags, const std::vector<std::string> &queries, bool recursive = false, bool overwrite = false, bool non_mp_globals = false, ReportingLevel reporting_level = ReportingLevel::REPORTING_LEVEL_ALL);
+        
     private:
-        ExtractionWorkload() = default;
+        /**
+         * Extract a tag from the map
+         * @param tag tag to extract
+         */
+        std::optional<std::unique_ptr<Parser::ParserStruct>> extract_tag(std::size_t tag_index);
+        
+        /**
+         * Perform the extraction
+         * @param queries        queries to do
+         * @param tags           tag files to output to
+         * @param recursive      also extract tags depended by a tag
+         * @param overwrite      overwrite tag files that exist
+         * @param non_mp_globals allow extraction of non-multiplayer globals
+         * @return               number of tags successfully extracted
+         */
+        std::size_t perform_extraction(const std::vector<std::string> &queries, const std::filesystem::path &tags, bool recursive, bool overwrite, bool non_mp_globals);
+        
+        /** Map reference */
+        const Map &map;
+        
+        /** Tags directory */
+        std::filesystem::path tags;
+        
+        /** All tags that were matched */
+        std::vector<std::size_t> matched_tags;
+        
+        ExtractionWorkload(const Map &map, ReportingLevel reporting_level);
         ~ExtractionWorkload() override = default;
     };
 }
--- src/extract/extraction.cpp
+++ src/extract/extraction.cpp
@@ -1,19 +1,337 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <regex>
+#include <invader/build/build_workload.hpp>
 #include <invader/extract/extraction.hpp>
 #include <invader/tag/hek/header.hpp>
 #include <invader/tag/parser/parser.hpp>
 
+static bool string_matches(const char *string, const char *pattern) {
+    for(const char *p = pattern;; p++) {
+        if(*p == *string && *p == 0) {
+            return true;
+        }
+        else if(*p == '?' || *p == *string) {
+            string++;
+            continue;
+        }
+        else if(*p == '*') {
+            p++;
+            if(*p == 0) {
+                return true;
+            }
+            for(const char *s = string; *s; s++) {
+                if(string_matches(s, p)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
 namespace Invader {
-    /**
-     * Extract the tag into an HEK tag file
-     * @param tag tag to extract
-     */
-    std::vector<std::byte> ExtractionWorkload::extract_tag(const Tag &tag) {
+    void ExtractionWorkload::extract_map(const Map &map, const std::string &tags, const std::vector<std::string> &queries, bool recursive, bool overwrite, bool non_mp_globals, ReportingLevel reporting_level) {
+        ExtractionWorkload workload(map, reporting_level);
+        auto start = std::chrono::steady_clock::now();
+        auto success = workload.perform_extraction(queries, tags, recursive, overwrite, non_mp_globals);
+        auto matched = workload.matched_tags.size();
+        auto warnings = workload.get_warnings();
+        auto errors = workload.get_errors();
+        
+        char initial_message[256] = {};
+        std::snprintf(initial_message, sizeof(initial_message), "Extracted %zu out of %zu matched tag%s", success, matched, matched == 1 ? "" : "s");
+        
+        char warnings_message[256] = {};
+        char errors_message[256] = {};
+        if(warnings) {
+            std::snprintf(warnings_message, sizeof(warnings_message), " with %zu warning%s", warnings, warnings == 1 ? "" : "s");
+        }
+        if(errors) {
+            std::snprintf(errors_message, sizeof(errors_message), "%s %zu error%s", warnings ? " and" : " with", errors, errors == 1 ? "" : "s");
+        }
+        
+        char timer[256] = {};
+        auto end = std::chrono::steady_clock::now();
+        std::snprintf(timer, sizeof(timer), " in %zu ms", std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+        
+        if(errors) {
+            oprintf_success_warn("%s%s%s%s", initial_message, warnings_message, errors_message, timer);
+        }
+        else if(warnings) {
+            oprintf_success_lesser_warn("%s%s%s", initial_message, warnings_message, timer);
+        }
+        else {
+            oprintf_success("%s%s", initial_message, timer);
+        }
+    }
+    
+    std::size_t ExtractionWorkload::perform_extraction(const std::vector<std::string> &queries, const std::filesystem::path &tags, bool recursive, bool overwrite, bool non_mp_globals) {
+        // Set these variables up
+        auto *map = &this->map;
+        auto engine = map->get_engine();
+        auto type = map->get_type();
+        auto tag_count = map->get_tag_count();
+        bool using_ipak = map->get_ipak_data().size();
+        std::vector<bool> extracted_tags(tag_count);
+        std::deque<std::size_t> all_tags_to_extract;
+        auto &workload = *this;
+
+        auto extract_tag = [&extracted_tags, &map, &tags, &all_tags_to_extract, &engine, &type, &using_ipak, &recursive, &overwrite, &non_mp_globals, &workload](std::size_t tag_index) -> bool {
+            // Used for bad paths
+            static const std::regex BAD_PATH_DIRECTORY("(^|.*(\\\\|\\/))\\.{1,2}(\\\\|\\/).*");
+
+            // Do it
+            extracted_tags[tag_index] = true;
+
+            // Get the tag path
+            const auto &tag = map->get_tag(tag_index);
+
+            if(engine == HEK::CacheFileEngine::CACHE_FILE_ANNIVERSARY) {
+                if(tag.get_tag_class_int() == TagClassInt::TAG_CLASS_SOUND) {
+                    eprintf_warn("CE Anniversary sounds cannot be extracted at this time");
+                    return false;
+                }
+                
+                if(tag.get_tag_class_int() == TagClassInt::TAG_CLASS_BITMAP && !using_ipak) {
+                    eprintf_warn("CE Anniversary bitmaps require an ipak to successfully extract");
+                    return false;
+                }
+            }
+
+            if(engine == HEK::CacheFileEngine::CACHE_FILE_XBOX && tag.get_tag_class_int() == TagClassInt::TAG_CLASS_BITMAP) {
+                eprintf_warn("Xbox bitmaps cannot be extracted at this time");
+                return false;
+            }
+
+            // See if we can extract this
+            auto tag_class_int = tag.get_tag_class_int();
+            const char *tag_extension = Invader::HEK::tag_class_to_extension(tag_class_int);
+            if(!tag.data_is_available()) {
+                return false;
+            }
+
+            // Get the path
+            auto path = Invader::File::halo_path_to_preferred_path(tag.get_path());
+
+            // Lowercase everything
+            for(char &c : path) {
+                c = std::tolower(c);
+            }
+
+            // Make sure we don't have any dot slashes (i.e. ../ or ./) to prevent a potential directory traversal attack
+            if(std::regex_match(path, BAD_PATH_DIRECTORY)) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "%s.%s contains an unsafe path", path.c_str(), tag_extension);
+                return false;
+            }
+
+            // Figure out the path we're writing to
+            auto tag_path_to_write_to = tags / (path + "." + tag_extension);
+            if(!overwrite && std::filesystem::exists(tag_path_to_write_to)) {
+                return false;
+            }
+
+            // Skip globals
+            if(tag_class_int == Invader::TagClassInt::TAG_CLASS_GLOBALS && !non_mp_globals && type != Invader::HEK::CacheFileType::SCENARIO_TYPE_MULTIPLAYER) {
+                workload.report_error(ErrorType::ERROR_TYPE_WARNING_PEDANTIC, "Skipping the non-multiplayer map's globals tag", tag_index);
+                return false;
+            }
+
+            // Get the tag data
+            std::vector<std::byte> new_tag;
+            try {
+                new_tag = Invader::ExtractionWorkload::extract_single_tag(tag);
+
+                // If we're recursive, we want to also get that stuff, too
+                if(recursive) {
+                    auto tag_compiled = BuildWorkload::compile_single_tag(new_tag.data(), new_tag.size(), std::vector<std::string>(), false);
+                    std::vector<std::pair<const std::string *, Invader::TagClassInt>> dependencies;
+                    for(auto &s : tag_compiled.structs) {
+                        for(auto &d : s.dependencies) {
+                            auto &tag = tag_compiled.tags[d.tag_index];
+                            dependencies.emplace_back(&tag.path, tag.tag_class_int);
+                        }
+                    }
+                    for(auto &d : dependencies) {
+                        auto tag_index = map->find_tag(d.first->c_str(), d.second);
+                        if(tag_index.has_value() && extracted_tags[*tag_index] == false) {
+                            all_tags_to_extract.push_back(*tag_index);
+                        }
+                    }
+                }
+            }
+            catch (std::exception &e) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Failed to extract %s.%s: %s", Invader::File::halo_path_to_preferred_path(tag.get_path()).c_str(), tag_extension, e.what());
+                return false;
+            }
+
+            // Jason Jones the tag
+            if(type == Invader::HEK::CacheFileType::SCENARIO_TYPE_SINGLEPLAYER) {
+                auto tag_path = tag.get_path();
+                bool changed = false;
+                switch(tag_class_int) {
+                    case Invader::TagClassInt::TAG_CLASS_WEAPON: {
+                        #define ALTER_TAG_DATA(from, to) if(from != to) { from = to; changed = true; } 
+                        
+                        if(tag_path == "weapons\\pistol\\pistol") {
+                            auto parsed = Invader::Parser::Weapon::parse_hek_tag_file(new_tag.data(), new_tag.size());
+                            if(parsed.triggers.size() >= 1) {
+                                auto &first_trigger = parsed.triggers[0];
+                                ALTER_TAG_DATA(first_trigger.minimum_error, DEGREES_TO_RADIANS(0.0F));
+                                ALTER_TAG_DATA(first_trigger.error_angle.from, DEGREES_TO_RADIANS(0.2F));
+                                ALTER_TAG_DATA(first_trigger.error_angle.to, DEGREES_TO_RADIANS(2.0F));
+                            }
+                            new_tag = parsed.generate_hek_tag_data(TagClassInt::TAG_CLASS_WEAPON);
+                        }
+                        else if(tag_path == "weapons\\plasma rifle\\plasma rifle") {
+                            auto parsed = Invader::Parser::Weapon::parse_hek_tag_file(new_tag.data(), new_tag.size());
+                            if(parsed.triggers.size() >= 1) {
+                                auto &first_trigger = parsed.triggers[0];
+                                ALTER_TAG_DATA(first_trigger.error_angle.from, DEGREES_TO_RADIANS(0.5F));
+                                ALTER_TAG_DATA(first_trigger.error_angle.to, DEGREES_TO_RADIANS(5.0F));
+                            }
+                            new_tag = parsed.generate_hek_tag_data(TagClassInt::TAG_CLASS_WEAPON);
+                        }
+                        break;
+                    }
+                    case Invader::TagClassInt::TAG_CLASS_DAMAGE_EFFECT:
+                        if(tag_path == "weapons\\pistol\\bullet") {
+                            auto parsed = Invader::Parser::DamageEffect::parse_hek_tag_file(new_tag.data(), new_tag.size());
+                            ALTER_TAG_DATA(parsed.elite_energy_shield, 1.0F);
+                            new_tag = parsed.generate_hek_tag_data(TagClassInt::TAG_CLASS_DAMAGE_EFFECT);
+                        }
+                        break;
+                    default:
+                        break;
+                        
+                        #undef ALTER_TAG_DATA
+                }
+                
+                if(changed) {
+                    REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING_PEDANTIC, tag_index, "%s.%s was changed due to being altered in singleplayer", Invader::File::halo_path_to_preferred_path(tag.get_path()).c_str(), tag_extension);
+                }
+            }
+
+            // Create directories along the way
+            try {
+                if(!std::filesystem::exists(tag_path_to_write_to.parent_path())) {
+                    std::filesystem::create_directories(tag_path_to_write_to.parent_path());
+                }
+            }
+            catch(std::exception &e) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Failed to create a directory: %s", e.what());
+                return false;
+            }
+
+            // Save it
+            auto tag_path_str = tag_path_to_write_to.string();
+            if(!Invader::File::save_file(tag_path_str.c_str(), new_tag)) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_ERROR, tag_index, "Failed to save %s", tag_path_str.c_str());
+                return false;
+            }
+
+            return true;
+        };
+
+        // Extract each tag?
+        if(queries.size() == 0) {
+            for(std::size_t t = 0; t < tag_count; t++) {
+                all_tags_to_extract.push_back(t);
+            }
+        }
+
+        else {
+            std::size_t tag_count = map->get_tag_count();
+            for(std::size_t t = 0; t < tag_count; t++) {
+                // See if we already added
+                bool already_added = false;
+                for(auto &tag : all_tags_to_extract) {
+                    if(tag == t) {
+                        already_added = true;
+                        break;
+                    }
+                }
+                if(already_added) {
+                    continue;
+                }
+
+                const auto &tag = map->get_tag(t);
+                auto full_tag_path = Invader::File::halo_path_to_preferred_path(tag.get_path()) + "." + HEK::tag_class_to_extension(tag.get_tag_class_int());
+
+                for(auto &query : queries) {
+                    if(string_matches(full_tag_path.c_str(), query.c_str())) {
+                        all_tags_to_extract.emplace_back(t);
+                        break;
+                    }
+                }
+            }
+
+            if(all_tags_to_extract.size() == 0) {
+                workload.report_error(ErrorType::ERROR_TYPE_ERROR, "No tags were found with the given search parameter(s).");
+                return 0;
+            }
+        }
+
+        // Extract tags
+        std::size_t total = 0;
+        std::size_t extracted = 0;
+        while(all_tags_to_extract.size() > 0) {
+            std::size_t tag = all_tags_to_extract[0];
+            all_tags_to_extract.erase(all_tags_to_extract.begin());
+            if(extracted_tags[tag]) {
+                continue;
+            }
+            const auto &tag_map = map->get_tag(tag);
+            if(extract_tag(tag)) {
+                oprintf_success("Extracted %s.%s", Invader::File::halo_path_to_preferred_path(tag_map.get_path()).c_str(), HEK::tag_class_to_extension(tag_map.get_tag_class_int()));
+                extracted++;
+            }
+            else {
+                eprintf("Skipped %s.%s\n", Invader::File::halo_path_to_preferred_path(tag_map.get_path()).c_str(), HEK::tag_class_to_extension(tag_map.get_tag_class_int()));
+            }
+        }
+        
+        this->matched_tags.reserve(total);
+        for(std::size_t i = 0; i < tag_count; i++) {
+            if(extracted_tags[i]) {
+                this->matched_tags.push_back(i);
+            }
+        }
+        
+        return extracted;
+    }
+    
+    ExtractionWorkload::ExtractionWorkload(const Map &map, ReportingLevel reporting_level) : ErrorHandler(reporting_level), map(map) {
+        auto &paths = this->get_tag_paths();
+        auto tag_count = map.get_tag_count();
+        paths.reserve(tag_count);
+        for(std::size_t i = 0; i < tag_count; i++) {
+            auto &tag = map.get_tag(i);
+            paths.emplace_back(tag.get_path(), tag.get_tag_class_int());
+        }
+    }
+    
+    std::vector<std::byte> ExtractionWorkload::extract_single_tag(const Tag &tag, ReportingLevel reporting_level) {
+        ExtractionWorkload workload(tag.get_map(), reporting_level);
+        auto result = workload.extract_tag(tag.get_tag_index());
+        if(result.has_value()) {
+            return result->get()->generate_hek_tag_data(tag.get_tag_class_int());
+        }
+        else {
+            throw InvalidTagDataException();
+        }
+    }
+    
+    std::optional<std::unique_ptr<Parser::ParserStruct>> ExtractionWorkload::extract_tag(std::size_t tag_index) {
+        auto &tag = this->map.get_tag(tag_index);
         auto tag_class_int = tag.get_tag_class_int();
 
         #define EXTRACT_TAG_CLASS(class_struct, class_int) case TagClassInt::class_int: { \
-            return Parser::class_struct::parse_cache_file_data(tag).generate_hek_tag_data(TagClassInt::class_int); \
+            return std::make_unique<Parser::class_struct>(Parser::class_struct::parse_cache_file_data(tag)); \
         }
 
         switch(tag_class_int) {
@@ -99,14 +417,14 @@ namespace Invader {
 
             case TagClassInt::TAG_CLASS_SCENARIO_STRUCTURE_BSP: {
                 if(tag.get_map().get_engine() == HEK::CacheFileEngine::CACHE_FILE_DARK_CIRCLET) {
-                    return Parser::ScenarioStructureBSP::parse_cache_file_data(tag).generate_hek_tag_data(TagClassInt::TAG_CLASS_SCENARIO_STRUCTURE_BSP);
+                    return std::make_unique<Parser::ScenarioStructureBSP>(Parser::ScenarioStructureBSP::parse_cache_file_data(tag));
                 }
                 else {
                     HEK::TagFileHeader tag_data_header(TagClassInt::TAG_CLASS_SCENARIO_STRUCTURE_BSP);
                     std::vector<std::byte> data(reinterpret_cast<std::byte *>(&tag_data_header), reinterpret_cast<std::byte *>(&tag_data_header + 1));
                     auto sbsp_header_data = tag.get_base_struct<HEK::ScenarioStructureBSPCompiledHeader>();
                     auto sbsp_header_pointer = sbsp_header_data.pointer.read();
-                    return Parser::ScenarioStructureBSP::parse_cache_file_data(tag, sbsp_header_pointer).generate_hek_tag_data(TagClassInt::TAG_CLASS_SCENARIO_STRUCTURE_BSP);
+                    return std::make_unique<Parser::ScenarioStructureBSP>(Parser::ScenarioStructureBSP::parse_cache_file_data(tag, sbsp_header_pointer));
                 }
             }
 
@@ -126,8 +444,7 @@ namespace Invader {
                 break;
         }
 
-        eprintf_error("Tag class %s is unsupported", tag_class_to_extension(tag_class_int));
-
-        throw InvalidTagDataException();
+        REPORT_ERROR_PRINTF(*this, ERROR_TYPE_ERROR, tag_index, "Tag class %s is unsupported", tag_class_to_extension(tag_class_int));
+        return std::nullopt;
     }
 }
