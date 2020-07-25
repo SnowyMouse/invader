75821af090f81c1465eaa9306ea0708a4bdc6a03
Snowy <kavawuvi@tutanota.com>
2020-07-25 14:09:25 -0500

    Don't require it to exist
    If you want to be a memelord and use a file that actually ends with .shader_transparent_chicago.shader_model, use the Find dialog, you meme.

--- src/edit/qt/editor/widget/tag_editor_edit_widget.cpp
+++ src/edit/qt/editor/widget/tag_editor_edit_widget.cpp
@@ -864,14 +864,16 @@ namespace Invader::EditQt {
                         
                         // If we got a path, see if we can find it again
                         if(is_allowed) {
+                            // First, Jason Jones the path in, accounting for the extension
+                            dependency.path = split_path->path;
+                            dependency.tag_class_int = split_path->class_int;
+                            textbox->setText(split_path->path.c_str());
+                            textbox->setCursorPosition(textbox->text().size());
+                            reinterpret_cast<QComboBox *>(this->widgets[0])->setCurrentText(HEK::tag_class_to_extension(split_path->class_int));
+                            
+                            // Then look for it
                             for(auto &t : this->get_editor_window()->get_parent_window()->get_all_tags()) {
                                 if(t.tag_class_int == class_int && File::split_tag_class_extension(File::halo_path_to_preferred_path(t.tag_path)).value().path == split_path->path) {
-                                    // If we found it, change it
-                                    dependency.path = split_path->path;
-                                    dependency.tag_class_int = split_path->class_int;
-                                    textbox->setText(split_path->path.c_str());
-                                    textbox->setCursorPosition(textbox->text().size());
-                                    reinterpret_cast<QComboBox *>(this->widgets[0])->setCurrentText(HEK::tag_class_to_extension(split_path->class_int));
                                     found = true;
                                     break;
                                 }
