7da7c1623b3710772438470c4434fd92d139ab1e
Snowy <snowy@totodile.io>
2020-12-27 15:25:53 -0600

    Add option for Xbox map compilation

--- src/build/build.cpp
+++ src/build/build.cpp
@@ -72,7 +72,7 @@ int main(int argc, const char **argv) {
     options.emplace_back("always-index-tags", 'a', 0, "Always index tags when possible. This can speed up build time, but stock tags can't be modified.");
     options.emplace_back("quiet", 'q', 0, "Only output error messages.");
     options.emplace_back("info", 'i', 0, "Show credits, source info, and other info.");
-    options.emplace_back("game-engine", 'g', 1, "Specify the game engine. This option is required. Valid engines are: custom, demo, native, retail, mcc-custom", "<id>");
+    options.emplace_back("game-engine", 'g', 1, "Specify the game engine. This option is required. Valid engines are: custom, demo, native, retail, xbox, mcc-custom", "<id>");
     options.emplace_back("with-index", 'w', 1, "Use an index file for the tags, ensuring the map's tags are ordered in the same way.", "<file>");
     options.emplace_back("maps", 'm', 1, "Use the specified maps directory.", "<dir>");
     options.emplace_back("tags", 't', 1, "Use the specified tags directory. Use multiple times to add more directories, ordered by precedence.", "<dir>");
@@ -125,6 +125,9 @@ int main(int argc, const char **argv) {
                 }
                 else if(std::strcmp(arguments[0], "demo") == 0) {
                     build_options.engine = HEK::CacheFileEngine::CACHE_FILE_DEMO;
+                }
+                else if(std::strcmp(arguments[0], "xbox") == 0) {
+                    build_options.engine = HEK::CacheFileEngine::CACHE_FILE_XBOX;
                 }
                 else if(std::strcmp(arguments[0], "native") == 0) {
                     build_options.engine = HEK::CacheFileEngine::CACHE_FILE_NATIVE;
