0c73d90d358ed2de339b3f5b31437e4299b51ebe
Snowy <snowy@totodile.io>
2020-12-27 02:20:09 -0600

    Implement a better swizzle function

--- src/bitmap/swizzle.cpp
+++ src/bitmap/swizzle.cpp
@@ -4,73 +4,59 @@
 #include <vector>
 #include <cstdint>
 #include <cstring>
+#include <cstdlib>
 
 namespace Invader::Swizzle {
-    template<typename Pixel> static std::size_t swizzle_block_2x2(const Pixel *values_in, Pixel *values_out, std::size_t stride, std::size_t counter, bool deswizzle) {
-        if(!deswizzle) {
-            // values_in  = unswizzled
-            // values_out = swizzled
-            values_out[counter++] = values_in[0];
-            values_out[counter++] = values_in[1];
-            values_out[counter++] = values_in[0 + stride];
-            values_out[counter++] = values_in[1 + stride];
+    // From https://www.forceflow.be/2013/10/07/morton-encodingdecoding-through-bit-interleaving-implementations/
+    static constexpr std::uint64_t morton_encode_3d(unsigned int x, unsigned int y, unsigned int z) {
+        std::uint64_t answer = 0;
+        for (std::uint64_t i = 0; i < (sizeof(std::uint64_t) * 8)/3; ++i) {
+            std::uint64_t bit = static_cast<std::uint64_t>(1) << i;
+            answer |= ((x & bit) << 2*i) | ((y & bit) << (2*i + 1)) | ((z & bit) << (2*i + 2));
         }
-        else {
-            // values_in  = swizzled
-            // values_out = unswizzled
-            values_out[0] = values_in[counter++];
-            values_out[1] = values_in[counter++];
-            values_out[0 + stride] = values_in[counter++];
-            values_out[1 + stride] = values_in[counter++];
-        }
-        return counter;
+        return answer;
     }
-
-    template<typename Pixel> static std::size_t swizzle_block(const Pixel *values_in, Pixel *values_out, std::size_t width, std::size_t stride, std::size_t counter, bool deswizzle) {
-        if(width == 2) {
-            return swizzle_block_2x2(values_in, values_out, stride, counter, deswizzle);
+    
+    static constexpr std::uint64_t morton_encode_2d(unsigned int x, unsigned int y) {
+        std::uint64_t answer = 0;
+        for (std::uint64_t i = 0; i < (sizeof(std::uint64_t) * 8)/3; ++i) {
+            std::uint64_t bit = static_cast<std::uint64_t>(1) << i;
+            answer |= ((x & bit) << i) | ((y & bit) << (i + 1));
         }
-
-        std::size_t new_width = width / 2;
-
-        if(!deswizzle) {
-            counter = swizzle_block(values_in, values_out                                 , new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in + new_width                                 , values_out, new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in + stride * new_width                        , values_out, new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in + stride * new_width + new_width, values_out, new_width, stride, counter, deswizzle);
-        }
-        else {
-            counter = swizzle_block(values_in, values_out                                 , new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in, values_out + new_width                     , new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in, values_out + stride * new_width            , new_width, stride, counter, deswizzle);
-            counter = swizzle_block(values_in, values_out + stride * new_width + new_width, new_width, stride, counter, deswizzle);
-        }
-
-        return counter;
+        return answer;
     }
-
-    template <typename Pixel> static void swizzle(const Pixel *values_in, Pixel *values_out, std::size_t width, std::size_t height, bool deswizzle) {
-        // If height is <= 1 or width is <= 2, it's just a straight copy
-        if(width <= 2 || height <= 1) {
-            std::memcpy(values_out, values_in, width * height * sizeof(*values_in));
-            return;
-        }
-
-        // Also, if width is less than the height, we can subdivide
-        if(width < height) {
-            for(std::size_t y = 0; y < height; y += width) {
-                swizzle(values_in + y * width, values_out + y * width, width, width, deswizzle);
+    
+    template <typename Pixel> static void perform_swizzle(const Pixel *values_in, Pixel *values_out, std::size_t width, std::size_t height, std::size_t depth, bool deswizzle) {
+        if(height > width) {
+            auto stride = width * width * depth;
+            for(std::size_t i = 0; i < height / width; i++) {
+                perform_swizzle(values_in + stride * i, values_out + stride * i, width, width, depth, deswizzle);
             }
             return;
         }
-
-        std::size_t counter = 0;
-        for(std::size_t x = 0; x < width; x+=height) {
-            if(!deswizzle) {
-                counter = swizzle_block(values_in + x, values_out, height, width, counter, deswizzle);
-            }
-            else {
-                counter = swizzle_block(values_in, values_out + x, height, width, counter, deswizzle);
+        
+        auto size = (width * height * depth);
+        
+        for(std::size_t z = 0; z < depth; z++) {
+            for(std::size_t y = 0; y < height; y++) {
+                for(std::size_t x = 0; x < width; x++) {
+                    std::uint64_t m;
+                    if(depth == 1) {
+                        m = morton_encode_2d(x,y) % size;
+                    }
+                    else {
+                        m = morton_encode_3d(x,y,z) % size;
+                    }
+                    
+                    auto offset = x + y * width + z * width * height;
+                    
+                    if(deswizzle) {
+                        values_out[offset] = values_in[m];
+                    }
+                    else {
+                        values_out[m] = values_in[offset];
+                    }
+                }
             }
         }
     }
@@ -80,16 +66,16 @@ namespace Invader::Swizzle {
 
         switch(bits_per_pixel) {
             case 8:
-                swizzle(reinterpret_cast<const std::uint8_t *>(data), reinterpret_cast<std::uint8_t *>(output.data()), width, height, deswizzle);
+                perform_swizzle(reinterpret_cast<const std::uint8_t *>(data), reinterpret_cast<std::uint8_t *>(output.data()), width, height, depth, deswizzle);
                 break;
             case 16:
-                swizzle(reinterpret_cast<const std::uint16_t *>(data), reinterpret_cast<std::uint16_t *>(output.data()), width, height, deswizzle);
+                perform_swizzle(reinterpret_cast<const std::uint16_t *>(data), reinterpret_cast<std::uint16_t *>(output.data()), width, height, depth, deswizzle);
                 break;
             case 32:
-                swizzle(reinterpret_cast<const std::uint32_t *>(data), reinterpret_cast<std::uint32_t *>(output.data()), width, height, deswizzle);
+                perform_swizzle(reinterpret_cast<const std::uint32_t *>(data), reinterpret_cast<std::uint32_t *>(output.data()), width, height, depth, deswizzle);
                 break;
             case 64:
-                swizzle(reinterpret_cast<const std::uint64_t *>(data), reinterpret_cast<std::uint64_t *>(output.data()), width, height, deswizzle);
+                perform_swizzle(reinterpret_cast<const std::uint64_t *>(data), reinterpret_cast<std::uint64_t *>(output.data()), width, height, depth, deswizzle);
                 break;
         }
         return output;
