f4668e335911492376e4740e9ad9a57249f2821d
Snowy <snowy@totodile.io>
2021-01-15 01:06:25 -0600

    Fix squish libraries for static linking on Windows

--- src/invader.cmake
+++ src/invader.cmake
@@ -248,4 +248,4 @@ set_source_files_properties(src/bitmap/stb/stb_impl.c PROPERTIES COMPILE_FLAGS -
 include_directories(${CMAKE_CURRENT_BINARY_DIR} ${ZLIB_INCLUDE_DIRS})
 
 # Link against everything
-target_link_libraries(invader invader-bitmap-p8-palette ${CMAKE_THREAD_LIBS_INIT} zstd ${ZLIB_LIBRARIES} ${DEP_AUDIO_LIBRARIES} squish)
+target_link_libraries(invader invader-bitmap-p8-palette ${CMAKE_THREAD_LIBS_INIT} zstd ${ZLIB_LIBRARIES} ${DEP_AUDIO_LIBRARIES} ${SQUISH_LIBRARIES})
