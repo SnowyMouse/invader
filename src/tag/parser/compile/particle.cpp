b338d5b47759a9283dec1ff4f37c7c309313632d
Snowy <snowy@snowymouse.com>
2022-02-28 00:24:09 -0600

    Factor in sequence count

--- src/tag/parser/compile/particle.cpp
+++ src/tag/parser/compile/particle.cpp
@@ -4,7 +4,7 @@
 #include <invader/build/build_workload.hpp>
 
 namespace Invader::Parser {
-    static float get_bitmap_tag_pixel_size(BuildWorkload &workload, std::size_t bitmap_tag_index, std::size_t sequence_index, bool &warn) {
+    static float get_bitmap_tag_pixel_size(BuildWorkload &workload, std::size_t bitmap_tag_index, std::size_t sequence_index, std::size_t sequence_count, bool &warn) {
         warn = false;
         
         if(workload.disable_recursion) {
@@ -29,34 +29,45 @@ namespace Invader::Parser {
 
             // Get sequences
             auto sequence_offset = bitmap_tag_struct.resolve_pointer(&bitmap_tag_data.bitmap_group_sequence.pointer);
+            auto bitmap_sequence_count = static_cast<std::size_t>(bitmap_tag_data.bitmap_group_sequence.count);
+            
             if(sequence_offset.has_value() && sequence_index < bitmap_tag_data.bitmap_group_sequence.count) {
                 auto &sequences_struct = workload.structs[*sequence_offset];
                 auto *sequences = reinterpret_cast<BitmapGroupSequence::struct_little *>(sequences_struct.data.data());
+                
+                auto current_index = sequence_index;
+                auto remaining_sequences = sequence_count;
+                
+                while(remaining_sequences > 0) {
+                    auto &sequence = sequences[current_index];
+                    auto sprites_offset = sequences_struct.resolve_pointer(&sequence.sprites.pointer);
 
-                auto &sequence = sequences[sequence_index];
-                auto sprites_offset = sequences_struct.resolve_pointer(&sequence.sprites.pointer);
+                    if(sprites_offset.has_value()) {
+                        auto *sprites = reinterpret_cast<BitmapGroupSprite::struct_little *>(workload.structs[*sprites_offset].data.data());
 
-                if(sprites_offset.has_value()) {
-                    auto *sprites = reinterpret_cast<BitmapGroupSprite::struct_little *>(workload.structs[*sprites_offset].data.data());
+                        // We'll need to iterate through all of the sprites
+                        std::size_t sprite_count = sequence.sprites.count;
+                        for(std::size_t i = 0; i < sprite_count; i++) {
+                            auto &sprite = sprites[i];
 
-                    // We'll need to iterate through all of the sprites
-                    std::size_t sprite_count = sequence.sprites.count;
-                    for(std::size_t i = 0; i < sprite_count; i++) {
-                        auto &sprite = sprites[i];
+                            // Get yer values here. Get 'em while they're hot.
+                            float width_bitmap = 1.0F / std::fabs(sprite.right - sprite.left) / bitmap_dimensions[sprite.bitmap_index].first;
+                            float height_bitmap = 1.0F / std::fabs(sprite.bottom - sprite.top) / bitmap_dimensions[sprite.bitmap_index].second;
 
-                        // Get yer values here. Get 'em while they're hot.
-                        float width_bitmap = 1.0F / std::fabs(sprite.right - sprite.left) / bitmap_dimensions[sprite.bitmap_index].first;
-                        float height_bitmap = 1.0F / std::fabs(sprite.bottom - sprite.top) / bitmap_dimensions[sprite.bitmap_index].second;
-
-                        // There!
-                        float smaller = (height_bitmap > width_bitmap) ? width_bitmap : height_bitmap;
-                        if(pixel_size > smaller) {
-                            pixel_size = smaller;
+                            // There!
+                            pixel_size = std::min(pixel_size, std::min(height_bitmap, width_bitmap));
+                            
+                            if(bitmap_dimensions[sprite.bitmap_index].first != bitmap_dimensions[sprite.bitmap_index].second) {
+                                warn = true;
+                            }
                         }
-                        
-                        if(bitmap_dimensions[sprite.bitmap_index].first != bitmap_dimensions[sprite.bitmap_index].second) {
-                            warn = true;
-                        }
+                    }
+                    
+                    // Stop?
+                    current_index++;
+                    remaining_sequences--;
+                    if(current_index == bitmap_sequence_count) {
+                        break;
                     }
                 }
             }
@@ -83,7 +94,7 @@ namespace Invader::Parser {
         bool warn;
         
         auto &particle = *reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset);
-        this->sprite_size = get_bitmap_tag_pixel_size(workload, this->bitmap.tag_id.index, this->first_sequence_index, warn);
+        this->sprite_size = get_bitmap_tag_pixel_size(workload, this->bitmap.tag_id.index, this->first_sequence_index, static_cast<std::size_t>(this->initial_sequence_count) + static_cast<std::size_t>(this->looping_sequence_count) + static_cast<std::size_t>(this->final_sequence_count), warn);
         particle.sprite_size = this->sprite_size;
         particle.make_it_actually_work = 1;
         
@@ -94,7 +105,7 @@ namespace Invader::Parser {
     void WeatherParticleSystemParticleType::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t offset) {
         bool warn;
         auto &particle = *reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset);
-        this->sprite_size = get_bitmap_tag_pixel_size(workload, this->sprite_bitmap.tag_id.index, 0, warn);
+        this->sprite_size = get_bitmap_tag_pixel_size(workload, this->sprite_bitmap.tag_id.index, 0, 1, warn);
         particle.sprite_size = this->sprite_size;
         particle.not_broken = 1;
         
@@ -104,7 +115,7 @@ namespace Invader::Parser {
     }
     void ParticleSystemTypeParticleState::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         bool warn;
-        get_bitmap_tag_pixel_size(workload, this->bitmaps.tag_id.index, this->sequence_index, warn);
+        get_bitmap_tag_pixel_size(workload, this->bitmaps.tag_id.index, this->sequence_index, 1, warn);
         
         if(warn) {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->bitmaps.tag_id.index]);
