07b29791decb36d4c1b3e49a9c1fc10996a774e5
Snowy <snowy@snowymouse.com>
2022-03-05 23:36:36 -0600

    Generate characters that are missing

--- src/tag/parser/compile/font.cpp
+++ src/tag/parser/compile/font.cpp
@@ -9,33 +9,34 @@ namespace Invader::Parser {
     #define CALCULATE_LEADING_WIDTH(font) ((static_cast<std::int32_t>((font).ascending_height) + static_cast<std::int32_t>((font).descending_height)) / 5)
     
     void generate_character_tables(Font &font) {
-        // Clear the character tables
-        font.character_tables.clear();
-
-        // Resize the character table table to 256
         static constexpr std::uint16_t CHARACTER_TABLE_COUNT = 256;
         static constexpr std::uint16_t CHARACTER_TABLE_SIZE = 256;
+        
+        // Resize the base table
         font.character_tables.resize(CHARACTER_TABLE_COUNT);
-
-        // Figure out how many characters we can address
-        const std::size_t CHARACTER_SIZE = font.characters.size();
-        const std::uint16_t CHARACTER_COUNT = CHARACTER_SIZE > (UINT16_MAX - 1) ? (UINT16_MAX - 1) : CHARACTER_SIZE;
-
-        // Go through it all
-        for(std::uint16_t i = 0; i < CHARACTER_COUNT; i++) {
-            auto character_index = font.characters[i].character;
-            std::size_t table = (character_index & 0xFF00) >> 8;
-            std::size_t index = (character_index & 0xFF);
-            auto &ctable = font.character_tables[table];
-            if(ctable.character_table.size() == 0) {
-                ctable.character_table.resize(CHARACTER_TABLE_SIZE);
-
-                for(std::uint16_t c = 0; c < CHARACTER_TABLE_SIZE; c++) {
-                    // Now we need to look for the character; set to null for now
-                    ctable.character_table[c].character_index = NULL_INDEX;
+        
+        // Get the character count
+        std::size_t character_count = std::min(font.characters.size(), static_cast<std::size_t>(NULL_INDEX - 1));
+        
+        // Fill anything in that was missing
+        for(std::size_t t = 0; t < CHARACTER_TABLE_COUNT; t++) {
+            auto &table = font.character_tables[t].character_table;
+            auto last_index = table.size();
+            table.resize(CHARACTER_TABLE_SIZE);
+            
+            if(last_index < CHARACTER_TABLE_SIZE) {
+                for(std::size_t i = last_index; i < CHARACTER_TABLE_SIZE; i++) {
+                    std::uint16_t character_index = (i & 0xFF) | ((t << 8) & 0xFF00);
+                    auto &value = table[i].character_index;
+                    value = NULL_INDEX;
+                    for(std::size_t c = 0; c < character_count; c++) {
+                        if(font.characters[c].character == character_index) {
+                            value = c;
+                            break;
+                        }
+                    }
                 }
             }
-            font.character_tables[table].character_table[index].character_index = i;
         }
     }
     
