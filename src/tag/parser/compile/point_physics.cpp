c1f425cdc5bd60430797e87dd205b3255c68e0b2
Snowy <snowy@snowymouse.com>
2022-03-11 14:43:21 -0600

    Modify point_physics calculations slightly
    This should be a bit closer to tool's precision. I don't know what Bungie did exactly here.

--- src/tag/parser/compile/point_physics.cpp
+++ src/tag/parser/compile/point_physics.cpp
@@ -7,9 +7,9 @@ namespace Invader::Parser {
         this->air_friction = this->air_friction * 10000.0f;
         this->water_friction = this->water_friction * 10000.0f;
 
-        this->unknown_constant = density * 118613.3333333f;
-        auto density = this->density;
-        this->water_gravity_scale = -1.0f * (density / WATER_DENSITY - 1.0f) / (density / WATER_DENSITY + 1.0f);
-        this->air_gravity_scale = -1.0f * (density / AIR_DENSITY - 1.0f) / (density / AIR_DENSITY + 1.0f);
+        this->unknown_constant = this->density * (3558400.0 / TICK_RATE);
+        
+        this->water_gravity_scale = (1.0 + this->density * (-1.0F / WATER_DENSITY)) / (1.0 + this->density * (+1.0F / WATER_DENSITY));
+        this->air_gravity_scale = (1.0 + this->density * (-1.0F / AIR_DENSITY)) / (1.0 + this->density * (+1.0F / AIR_DENSITY));
     }
 }
