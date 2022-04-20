5cb166c1003c72a0f0e8b60ec91a30897c026119
Snowy <snowy@snowymouse.com>
2022-04-19 22:41:58 -0500

    Refactor button type to a header file

---
+++ src/string/button_type.hpp
@@ -0,0 +1,49 @@
+#ifndef INVADER__STRING__BUTTON_TYPE_HPP
+#define INVADER__STRING__BUTTON_TYPE_HPP
+
+namespace Invader {
+    static constexpr const char16_t *ALL_MESSAGE_TYPES[] = {
+        u"a-button",
+        u"b-button",
+        u"x-button",
+        u"y-button",
+        u"black-button",
+        u"white-button",
+        u"left-trigger",
+        u"right-trigger",
+        u"dpad-up",
+        u"dpad-down",
+        u"dpad-left",
+        u"dpad-right",
+        u"start-button",
+        u"back-button",
+        u"left-thumb",
+        u"right-thumb",
+        u"left-stick",
+        u"right-stick",
+        u"action",
+        u"throw-grenade",
+        u"primary-trigger",
+        u"integrated-light",
+        u"jump",
+        u"use-equipment",
+        u"rotate-weapons",
+        u"rotate-grenades",
+        u"crouch",
+        u"zoom",
+        u"accept",
+        u"back",
+        u"move",
+        u"look",
+        u"custom-1",
+        u"custom-2",
+        u"custom-3",
+        u"custom-4",
+        u"custom-5",
+        u"custom-6",
+        u"custom-7",
+        u"custom-8"
+    };
+}
+
+#endif
--- src/string/string.cpp
+++ src/string/string.cpp
@@ -11,6 +11,8 @@
 #include <invader/tag/parser/parser.hpp>
 #include <invader/file/file.hpp>
 
+#include "button_type.hpp"
+
 using namespace Invader;
 
 enum Format {
@@ -216,49 +218,6 @@ static std::vector<std::byte> generate_hud_message_text_tag(const std::u16string
             // Control
             if(*word_start == '%') {
                 std::u16string thing = { word_start + 1, c };
-                static constexpr const char16_t *ALL_MESSAGE_TYPES[] = {
-                    u"a-button",
-                    u"b-button",
-                    u"x-button",
-                    u"y-button",
-                    u"black-button",
-                    u"white-button",
-                    u"left-trigger",
-                    u"right-trigger",
-                    u"dpad-up",
-                    u"dpad-down",
-                    u"dpad-left",
-                    u"dpad-right",
-                    u"start-button",
-                    u"back-button",
-                    u"left-thumb",
-                    u"right-thumb",
-                    u"left-stick",
-                    u"right-stick",
-                    u"action",
-                    u"throw-grenade",
-                    u"primary-trigger",
-                    u"integrated-light",
-                    u"jump",
-                    u"use-equipment",
-                    u"rotate-weapons",
-                    u"rotate-grenades",
-                    u"crouch",
-                    u"zoom",
-                    u"accept",
-                    u"back",
-                    u"move",
-                    u"look",
-                    u"custom-1",
-                    u"custom-2",
-                    u"custom-3",
-                    u"custom-4",
-                    u"custom-5",
-                    u"custom-6",
-                    u"custom-7",
-                    u"custom-8"
-                };
-                
                 bool found = false;
                 for(std::size_t i = 0; i < sizeof(ALL_MESSAGE_TYPES) / sizeof(*ALL_MESSAGE_TYPES); i++) {
                     if(thing == ALL_MESSAGE_TYPES[i]) {
