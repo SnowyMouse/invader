71a2d0fd453c18ac9f23c0a4fa62a4c314aae557
Snowy <snowy@snowymouse.com>
2022-09-02 23:46:30 +0200

    check_invalid_ranges.py should handle raw data
    Signed-off-by: Vaporeon <vaporeon@vaporeon.io>

--- src/tag/code_generator/check_invalid_ranges.py
+++ src/tag/code_generator/check_invalid_ranges.py
@@ -10,13 +10,15 @@ def make_check_invalid_ranges(all_used_structs, struct_name, hpp, cpp_check_inva
             cpp_check_invalid_ranges.write("        for(auto &r : this->{}) {{\n".format(name))
             cpp_check_invalid_ranges.write("            return_value = r.check_for_invalid_ranges(clamp) || return_value;\n")
             cpp_check_invalid_ranges.write("        }\n")
+        elif struct["type"] == "TagDataOffset":
+            pass
         else:
             # Get the min/max values
             minimum = struct["minimum"] if "minimum" in struct else None
             maximum = struct["maximum"] if "maximum" in struct else None
             if minimum is None and maximum is None:
                 continue
-            
+
             def write_range_check(what):
                 if minimum != None:
                     cpp_check_invalid_ranges.write("        if({} < {}) {{\n".format(what, minimum))
@@ -34,12 +36,12 @@ def make_check_invalid_ranges(all_used_structs, struct_name, hpp, cpp_check_inva
                     cpp_check_invalid_ranges.write("            {} = {};\n".format(what, maximum))
                     cpp_check_invalid_ranges.write("            return_value = true;\n")
                     cpp_check_invalid_ranges.write("        }\n")
-            
+
             if ("bounds" in struct) and struct["bounds"]:
                 write_range_check("this->{}.from".format(name))
                 write_range_check("this->{}.to".format(name))
             else:
                 write_range_check("this->{}".format(name))
-        
+
     cpp_check_invalid_ranges.write("        return return_value;\n")
     cpp_check_invalid_ranges.write("    }\n")
