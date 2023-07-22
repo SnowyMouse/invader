178e4cca82c1b9264a22db12eb8bdcaa5d793cc0
Aerocatia <aerocatia@tutanota.com>
2023-07-23 08:10:14 +1200

    It really did mean bitmap index

--- src/tag/parser/compile/lens_flare.cpp
+++ src/tag/parser/compile/lens_flare.cpp
@@ -3,8 +3,6 @@
 #include <invader/build/build_workload.hpp>
 #include <invader/tag/parser/parser.hpp>
 
-#include "hud_interface.hpp"
-
 namespace Invader::Parser {
     void LensFlare::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         this->cos_falloff_angle = std::cos(this->falloff_angle);
@@ -16,12 +14,27 @@ namespace Invader::Parser {
     }
 
     void LensFlare::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
-        auto reflection_count = this->reflections.size();
-        for(std::size_t i = 0; i < reflection_count; i++) {
-            auto &reflection = this->reflections[i];
-            char str[256];
-            std::snprintf(str, sizeof(str), "reflection #%zu", i);
-            CHECK_BITMAP_SEQUENCE(workload, this->bitmap, reflection.bitmap_index, str);
+        // Bounds check the bitmap index. This does not use sequence index unlike most things.
+        if(!workload.disable_recursion && !workload.disable_error_checking) {
+            auto reflection_count = this->reflections.size();
+            auto lens_flare_bitmap = this->bitmap.tag_id;
+            if(!lens_flare_bitmap.is_null()) {
+                std::size_t bitmap_count;
+                bitmap_count = reinterpret_cast<Bitmap::struct_little *>(workload.structs[*workload.tags[lens_flare_bitmap.index].base_struct].data.data())->bitmap_data.count.read();
+                for(std::size_t i = 0; i < reflection_count; i++) {
+                    auto &reflection = this->reflections[i];
+                    if(reflection.bitmap_index >= bitmap_count) {
+                        char bitmap_path[256];
+                        std::snprintf(bitmap_path, sizeof(bitmap_path), "%s.%s", this->bitmap.path.c_str(), HEK::tag_fourcc_to_extension(this->bitmap.tag_fourcc));
+                        REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Bitmap #%zu in %s referenced in reflection #%zu is out of bounds (>= %zu)", static_cast<std::size_t>(reflection.bitmap_index), bitmap_path, i, bitmap_count);
+                        throw InvalidTagDataException();
+                    }
+                }
+            }
+            else if(reflection_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Lens flare has %zu reflections, but no bitmap is referenced", reflection_count);
+                throw InvalidTagDataException();
+            }
         }
     }
 }
