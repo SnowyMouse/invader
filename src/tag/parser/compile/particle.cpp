202a390c5f72fa6f11e486766c0f24663d8235cc
Snowy <snowy@snowymouse.com>
2023-07-23 21:04:31 +0200

    Check render mode when checking sequences for particle systems

--- src/tag/parser/compile/particle.cpp
+++ src/tag/parser/compile/particle.cpp
@@ -6,7 +6,7 @@
 namespace Invader::Parser {
     static float get_bitmap_tag_pixel_size(BuildWorkload &workload, std::size_t bitmap_tag_index, std::size_t sequence_index, std::size_t sequence_count, bool &warn) {
         warn = false;
-        
+
         if(workload.disable_recursion) {
             return 1.0F;
         }
@@ -30,14 +30,14 @@ namespace Invader::Parser {
             // Get sequences
             auto sequence_offset = bitmap_tag_struct.resolve_pointer(&bitmap_tag_data.bitmap_group_sequence.pointer);
             auto bitmap_sequence_count = static_cast<std::size_t>(bitmap_tag_data.bitmap_group_sequence.count);
-            
+
             if(sequence_offset.has_value() && sequence_index < bitmap_tag_data.bitmap_group_sequence.count) {
                 auto &sequences_struct = workload.structs[*sequence_offset];
                 auto *sequences = reinterpret_cast<BitmapGroupSequence::struct_little *>(sequences_struct.data.data());
-                
+
                 auto current_index = sequence_index;
                 auto remaining_sequences = sequence_count;
-                
+
                 while(remaining_sequences > 0) {
                     auto &sequence = sequences[current_index];
                     auto sprites_offset = sequences_struct.resolve_pointer(&sequence.sprites.pointer);
@@ -56,13 +56,13 @@ namespace Invader::Parser {
 
                             // There!
                             pixel_size = std::min(pixel_size, std::min(height_bitmap, width_bitmap));
-                            
+
                             if(bitmap_dimensions[sprite.bitmap_index].first != bitmap_dimensions[sprite.bitmap_index].second) {
                                 warn = true;
                             }
                         }
                     }
-                    
+
                     // Stop?
                     current_index++;
                     remaining_sequences--;
@@ -77,7 +77,7 @@ namespace Invader::Parser {
 
     void Particle::postprocess_hek_data() {
         this->contact_deterioration = 0.0F;
-        
+
         if(this->radius_animation.from == 0.0F) {
             this->radius_animation.from = 1.0F;
         }
@@ -85,19 +85,19 @@ namespace Invader::Parser {
             this->radius_animation.to = 1.0F;
         }
     }
-    
+
     static void complain_about_non_square_sheets(BuildWorkload &workload, std::size_t tag_index, const BuildWorkload::BuildWorkloadTag &tag) {
         REPORT_ERROR_PRINTF(workload, ERROR_TYPE_WARNING, tag_index, "Bitmap %s.%s uses non-square sprite sheets. The particle will be distorted.", File::halo_path_to_preferred_path(tag.path).c_str(), HEK::tag_fourcc_to_extension(tag.tag_fourcc));
     }
-    
+
     void Particle::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t offset) {
         bool warn;
-        
+
         auto &particle = *reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset);
         this->sprite_size = get_bitmap_tag_pixel_size(workload, this->bitmap.tag_id.index, this->first_sequence_index, static_cast<std::size_t>(this->initial_sequence_count) + static_cast<std::size_t>(this->looping_sequence_count) + static_cast<std::size_t>(this->final_sequence_count), warn);
         particle.sprite_size = this->sprite_size;
         particle.make_it_actually_work = 1;
-        
+
         if(warn) {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->bitmap.tag_id.index]);
         }
@@ -108,7 +108,7 @@ namespace Invader::Parser {
         this->sprite_size = get_bitmap_tag_pixel_size(workload, this->sprite_bitmap.tag_id.index, 0, 1, warn);
         particle.sprite_size = this->sprite_size;
         particle.not_broken = 1;
-        
+
         if(warn) {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->sprite_bitmap.tag_id.index]);
         }
@@ -116,12 +116,69 @@ namespace Invader::Parser {
     void ParticleSystemTypeParticleState::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         bool warn;
         get_bitmap_tag_pixel_size(workload, this->bitmaps.tag_id.index, this->sequence_index, 1, warn);
-        
+
         if(warn) {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->bitmaps.tag_id.index]);
         }
     }
     void ParticleSystemTypeParticleState::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
         this->unknown_int = 1;
+    }
+    void ParticleSystemType::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t struct_offset) {
+        if(workload.disable_recursion) {
+            return;
+        }
+
+        auto particle_state_count = this->particle_states.size();
+
+        if(particle_state_count == 0) {
+            return;
+        }
+
+        std::size_t required_bitmaps_in_sequence;
+
+        if(this->complex_sprite_render_modes == HEK::ParticleSystemComplexSpriteRenderModes::PARTICLE_SYSTEM_COMPLEX_SPRITE_RENDER_MODES_SIMPLE) {
+            required_bitmaps_in_sequence = 1;
+        }
+        else if(this->complex_sprite_render_modes == HEK::ParticleSystemComplexSpriteRenderModes::PARTICLE_SYSTEM_COMPLEX_SPRITE_RENDER_MODES_ROTATIONAL) {
+            required_bitmaps_in_sequence = 2;
+        }
+        else {
+            std::terminate(); // unreachable
+        }
+
+        auto &particle_system_type_struct = workload.structs[struct_index];
+        auto &particle_system_type_struct_c = *reinterpret_cast<struct_little *>(particle_system_type_struct.data.data() + struct_offset);
+        auto particle_state_struct_index = particle_system_type_struct.resolve_pointer(&particle_system_type_struct_c.particle_states.pointer).value();
+        auto &particle_state_struct = workload.structs[particle_state_struct_index];
+        auto *particle_state_struct_c = reinterpret_cast<ParticleSystemTypeParticleState::struct_little *>(particle_state_struct.data.data());
+        for(std::size_t i = 0; i < particle_state_count; i++) {
+            auto &state = particle_state_struct_c[i];
+            auto bitmap_tag_id = state.bitmaps.tag_id.read();
+
+            auto &bitmap_tag = workload.tags[bitmap_tag_id.index];
+            auto &bitmap_tag_struct = workload.structs[*bitmap_tag.base_struct];
+            auto &bitmap_tag_data = *reinterpret_cast<Bitmap::struct_little *>(bitmap_tag_struct.data.data());
+
+            if(bitmap_tag_data.type != HEK::BitmapType::BITMAP_TYPE_SPRITES) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Bitmap %s.%s is not type sprites. Particle states only accept sprites.", File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc));
+                throw InvalidTagDataException();
+            }
+
+            auto sequence_index = static_cast<std::size_t>(state.sequence_index);
+            auto sequence_count = static_cast<std::size_t>(bitmap_tag_data.bitmap_group_sequence.count);
+            if(sequence_index >= sequence_count) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Sequence index is out-of-bounds for bitmap %s.%s (%zu >= %zu)", File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), sequence_index, sequence_count);
+                throw InvalidTagDataException();
+            }
+
+            auto sequence_struct_index = bitmap_tag_struct.resolve_pointer(&bitmap_tag_data.bitmap_group_sequence.pointer).value();
+            auto &sequence_struct = reinterpret_cast<BitmapGroupSequence::struct_little *>(workload.structs[sequence_struct_index].data.data())[sequence_index];
+            auto actual_bitmaps_in_sequence = static_cast<std::size_t>(sequence_struct.sprites.count);
+            if(actual_bitmaps_in_sequence < required_bitmaps_in_sequence) {
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Insufficient sprite count for render mode in sequence #%zu of bitmap %s.%s (%zu < %zu)", sequence_index, File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), actual_bitmaps_in_sequence, required_bitmaps_in_sequence);
+                throw InvalidTagDataException();
+            }
+        }
     }
 }
