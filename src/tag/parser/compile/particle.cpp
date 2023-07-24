2e9a8a818be5cb16c49aca715b8640965f4ce1f8
Aerocatia <aerocatia@tutanota.com>
2023-07-24 13:04:16 +1200

    This check was incorrect, it needs an extra sequence not an extra sprite

--- src/tag/parser/compile/particle.cpp
+++ src/tag/parser/compile/particle.cpp
@@ -102,6 +102,7 @@ namespace Invader::Parser {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->bitmap.tag_id.index]);
         }
     }
+
     void WeatherParticleSystemParticleType::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t offset) {
         bool warn;
         auto &particle = *reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + offset);
@@ -113,6 +114,7 @@ namespace Invader::Parser {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->sprite_bitmap.tag_id.index]);
         }
     }
+
     void ParticleSystemTypeParticleState::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
         bool warn;
         get_bitmap_tag_pixel_size(workload, this->bitmaps.tag_id.index, this->sequence_index, 1, warn);
@@ -121,9 +123,11 @@ namespace Invader::Parser {
             complain_about_non_square_sheets(workload, tag_index, workload.tags[this->bitmaps.tag_id.index]);
         }
     }
+
     void ParticleSystemTypeParticleState::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
         this->unknown_int = 1;
     }
+
     void ParticleSystemType::post_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t struct_index, std::size_t struct_offset) {
         if(workload.disable_recursion) {
             return;
@@ -135,17 +139,7 @@ namespace Invader::Parser {
             return;
         }
 
-        std::size_t required_bitmaps_in_sequence;
-
-        if(this->complex_sprite_render_modes == HEK::ParticleSystemComplexSpriteRenderModes::PARTICLE_SYSTEM_COMPLEX_SPRITE_RENDER_MODES_SIMPLE) {
-            required_bitmaps_in_sequence = 1;
-        }
-        else if(this->complex_sprite_render_modes == HEK::ParticleSystemComplexSpriteRenderModes::PARTICLE_SYSTEM_COMPLEX_SPRITE_RENDER_MODES_ROTATIONAL) {
-            required_bitmaps_in_sequence = 2;
-        }
-        else {
-            std::terminate(); // unreachable
-        }
+        bool rotational = this->complex_sprite_render_mode == HEK::ParticleSystemComplexSpriteRenderMode::PARTICLE_SYSTEM_COMPLEX_SPRITE_RENDER_MODE_ROTATIONAL;
 
         auto &particle_system_type_struct = workload.structs[struct_index];
         auto &particle_system_type_struct_c = *reinterpret_cast<struct_little *>(particle_system_type_struct.data.data() + struct_offset);
@@ -161,23 +155,22 @@ namespace Invader::Parser {
             auto &bitmap_tag_data = *reinterpret_cast<Bitmap::struct_little *>(bitmap_tag_struct.data.data());
 
             if(bitmap_tag_data.type != HEK::BitmapType::BITMAP_TYPE_SPRITES) {
-                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Bitmap %s.%s is not type sprites. Particle states only accept sprites.", File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc));
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Bitmap %s.%s referenced in particle state #%zu of particle type #%zu is not of type sprites. Particle states only accept sprites.", File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), i, struct_offset / sizeof(ParticleSystemType::struct_little));
                 throw InvalidTagDataException();
             }
 
             auto sequence_index = static_cast<std::size_t>(state.sequence_index);
             auto sequence_count = static_cast<std::size_t>(bitmap_tag_data.bitmap_group_sequence.count);
             if(sequence_index >= sequence_count) {
-                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Sequence index is out-of-bounds for bitmap %s.%s (%zu >= %zu)", File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), sequence_index, sequence_count);
+                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Sequence #%zu in %s.%s referenced in particle state #%zu of particle type #%zu is out of bounds (>= %zu)", sequence_index, File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), i, struct_offset / sizeof(ParticleSystemType::struct_little), sequence_count);
                 throw InvalidTagDataException();
             }
-
-            auto sequence_struct_index = bitmap_tag_struct.resolve_pointer(&bitmap_tag_data.bitmap_group_sequence.pointer).value();
-            auto &sequence_struct = reinterpret_cast<BitmapGroupSequence::struct_little *>(workload.structs[sequence_struct_index].data.data())[sequence_index];
-            auto actual_bitmaps_in_sequence = static_cast<std::size_t>(sequence_struct.sprites.count);
-            if(actual_bitmaps_in_sequence < required_bitmaps_in_sequence) {
-                REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Insufficient sprite count for render mode in sequence #%zu of bitmap %s.%s (%zu < %zu)", sequence_index, File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), actual_bitmaps_in_sequence, required_bitmaps_in_sequence);
-                throw InvalidTagDataException();
+            if(rotational) {
+                auto extra_sequence_index = sequence_index + 1;
+                if(extra_sequence_index >= sequence_count) {
+                    REPORT_ERROR_PRINTF(workload, ERROR_TYPE_FATAL_ERROR, tag_index, "Particle type #%zu uses complex sprite render mode \'rotational\' that requires extra sequence #%zu in bitmap %s.%s referenced in particle state #%zu, but this is out of bounds (>= %zu)", struct_offset / sizeof(ParticleSystemType::struct_little), extra_sequence_index, File::halo_path_to_preferred_path(bitmap_tag.path).c_str(), HEK::tag_fourcc_to_extension(bitmap_tag.tag_fourcc), i, sequence_count);
+                    throw InvalidTagDataException();
+                }
             }
         }
     }
