81321617a825b882335f49a4941cb15c3426572c
Aerocatia <aerocatia@tutanota.com>
2024-08-01 08:28:01 +1200

    rename unknown constant to mass scale

--- src/tag/parser/compile/point_physics.cpp
+++ src/tag/parser/compile/point_physics.cpp
@@ -4,11 +4,11 @@
 
 namespace Invader::Parser {
     void PointPhysics::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
-        this->air_friction = this->air_friction * 10000.0f;
-        this->water_friction = this->water_friction * 10000.0f;
+        this->air_friction = this->air_friction * 10000.0F;
+        this->water_friction = this->water_friction * 10000.0F;
 
-        this->unknown_constant = this->density * (3558400.0 / TICK_RATE);
-        
+        this->mass_scale = this->density * 118613.34F;
+
         this->water_gravity_scale = (1.0 + this->density * (-1.0F / WATER_DENSITY)) / (1.0 + this->density * (+1.0F / WATER_DENSITY));
         this->air_gravity_scale = (1.0 + this->density * (-1.0F / AIR_DENSITY)) / (1.0 + this->density * (+1.0F / AIR_DENSITY));
     }
