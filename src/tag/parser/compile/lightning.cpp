82a30b19faf09613b656adc5effbf4270a4dcbd7
Aerocatia <aerocatia@tutanota.com>
2024-12-15 09:49:30 +1300

    expose secondary bitmap struct in lightning tags
    It seems to work on Xbox and mcc-cea

--- src/tag/parser/compile/lightning.cpp
+++ src/tag/parser/compile/lightning.cpp
@@ -5,6 +5,5 @@
 namespace Invader::Parser {
     void LightningShader::pre_compile(BuildWorkload &, std::size_t, std::size_t, std::size_t) {
         this->shader_type = HEK::ShaderType::SHADER_TYPE_EFFECT;
-        this->some_more_stuff_that_should_be_set_for_some_reason = -1;
     }
 }
