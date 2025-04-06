aca0da6190b418356b32cf89005cb9558a1f2d25
Aerocatia <aerocatia@tutanota.com>
2025-04-06 22:03:44 +1200

    Update invader-crc usage to match the other tools

--- src/crc/crc.cpp
+++ src/crc/crc.cpp
@@ -1,28 +1,76 @@
 #include <cstdio>
 #include <memory>
 #include <invader/printf.hpp>
+#include <invader/version.hpp>
 #include <invader/map/map.hpp>
 #include <invader/crc/hek/crc.hpp>
+#include "../command_line_option.hpp"
 
 extern "C" {
     #include "crc_spoof.h"
     #include "crc32.h"
 }
 
-int main(int argc, const char **argv) {
-    using namespace Invader;
-
-    // Make sure an argument was passed. If not, give the usage
-    if(argc < 2 || argc > 3) {
-        eprintf("Usage: %s <map> [crc]\n", argv[0]);
-        return EXIT_FAILURE;
+static std::uint32_t read_str32(const char *err, const char *s) {
+    // Make sure it starts with '0x'
+    if(std::strncmp(s, "0x", 2) != 0) {
+        eprintf_error("%s %s (must be hexadecimal)", err, s);
+        std::exit(EXIT_FAILURE);
     }
 
+    // Check the string length
+    std::size_t given_crc32_length = std::strlen(s);
+    if(given_crc32_length > 10 || given_crc32_length < 3) {
+        eprintf_error("%s %s (must be 1-8 digits)", err, s);
+        std::exit(EXIT_FAILURE);
+    }
+
+    // Now, make sure it's all valid
+    for(std::size_t i = 2; i < given_crc32_length; i++) {
+        char c = std::tolower(s[i]);
+        if(!(c >= '0' && c <= '9') && !(c >= 'a' && c <= 'f')) {
+            eprintf_error("%s %s (must be hexadecimal)", err, s);
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
+    return static_cast<std::uint32_t>(std::strtoul(s + 2, nullptr, 16));
+}
+
+int main(int argc, const char **argv) {
+    set_up_color_term();
+
+    using namespace Invader;
+
+    const CommandLineOption options[] {
+        CommandLineOption::from_preset(CommandLineOption::PRESET_COMMAND_LINE_OPTION_INFO),
+        CommandLineOption("forge", 'f', 1, "Forge the CRC32 value of the map.", "<crc>")
+    };
+
+    static constexpr char DESCRIPTION[] = "Inspect or forge the CRC32 of a cache file.";
+    static constexpr char USAGE[] = "[options] <map>";
+
+    struct CrcOptions {
+        std::optional<std::uint32_t> forge_crc;
+    } crc_options;
+
+    auto remaining_arguments = CommandLineOption::parse_arguments<CrcOptions &>(argc, argv, options, USAGE, DESCRIPTION, 1, 1, crc_options, [](char opt, const auto &arguments, auto &crc_options) {
+        switch(opt) {
+            case 'i':
+                show_version_info();
+                std::exit(EXIT_SUCCESS);
+                break;
+            case 'f':
+                crc_options.forge_crc = read_str32("Invalid CRC32", arguments[0]);
+                break;
+        }
+    });
+
     // Open the map
-    const auto *file = argv[1];
+    const auto *file = remaining_arguments[0];
     std::FILE *f = std::fopen(file, "rb");
     if(!f) {
-        eprintf("Failed to open %s\n", file);
+        eprintf_error("Failed to open %s", file);
         return EXIT_FAILURE;
     }
     std::fseek(f, 0, SEEK_END);
@@ -32,14 +80,14 @@ int main(int argc, const char **argv) {
     // Hold the tag data
     std::unique_ptr<std::byte[]> data = std::make_unique<std::byte[]>(size);
     if(std::fread(data.get(), size, 1, f) != 1) {
-        eprintf("Failed to read %s\n", file);
+        eprintf_error("Failed to read %s", file);
         std::fclose(f);
         return EXIT_FAILURE;
     }
     std::fclose(f);
 
     if(size < sizeof(HEK::CacheFileHeader)) {
-        eprintf("%s is too small to be a valid cache file.\n", file);
+        eprintf_error("%s is too small to be a valid cache file.", file);
         return EXIT_FAILURE;
     }
 
@@ -59,7 +107,7 @@ int main(int argc, const char **argv) {
         HEK::CacheFileDemoHeader demo_header;
         std::memcpy(static_cast<void *>(&demo_header), data.get(), sizeof(HEK::CacheFileDemoHeader));
         if(!demo_header.valid()) {
-            eprintf("%s is not a valid cache file.\n", file);
+            eprintf_error("%s is not a valid cache file.", file);
             return EXIT_FAILURE;
         }
         has_demo_header = true;
@@ -69,34 +117,21 @@ int main(int argc, const char **argv) {
     }
 
     if(cache_version == HEK::CACHE_FILE_XBOX) {
-        eprintf("%s is an Xbox cache file and not supported by invader-crc.\n", file);
+        eprintf_error("%s is an Xbox cache file and not supported by invader-crc.", file);
         return EXIT_FAILURE;
     }
 
     if(size < tag_data_offset + tag_data_size) {
-        eprintf("Tag data size for %s is invalid.\n", file);
+        eprintf_error("Tag data size for %s is invalid.", file);
         return EXIT_FAILURE;
     }
 
     std::uint32_t final_crc = 0;
 
-    if(argc == 3) {
-        std::size_t given_crc32_length = std::strlen(argv[2]);
-        if(given_crc32_length > 8 || given_crc32_length < 1) {
-            eprintf("Invalid CRC32 %s (must be 1-8 digits)\n", argv[2]);
-            return EXIT_FAILURE;
-        }
-        for(std::size_t i = 0; i < given_crc32_length; i++) {
-            char c = std::tolower(argv[2][i]);
-            if(!(c >= '0' && c <= '9') && !(c >= 'a' && c <= 'f')) {
-                eprintf("Invalid CRC32 %s (must be hexadecimal)\n", argv[2]);
-                return EXIT_FAILURE;
-            }
-        }
-
-        std::uint32_t given_crc = std::strtoul(argv[2], nullptr, 16);
+    if(crc_options.forge_crc.has_value()) {
+        // Forge the CRC32 to a new value
         std::uint32_t checksum_delta;
-        final_crc = calculate_map_crc(data.get(), size, &given_crc, &checksum_delta);
+        final_crc = calculate_map_crc(data.get(), size, &crc_options.forge_crc.value(), &checksum_delta);
 
         std::size_t crc32_offset = offsetof(HEK::CacheFileHeader, crc32);
         if(has_demo_header) {
@@ -105,23 +140,26 @@ int main(int argc, const char **argv) {
 
         f = std::fopen(file, "rb+");
         if(!f) {
-            eprintf("Failed to open %s\n", file);
+            eprintf_error("Failed to open %s", file);
             return EXIT_FAILURE;
         }
 
         // Write header crc32
         std::fseek(f, crc32_offset, SEEK_SET);
         std::fwrite(&final_crc, sizeof(final_crc), 1, f);
+
         // Write delta to tag header
         std::fseek(f, tag_data_offset + offsetof(HEK::CacheFileTagDataHeader, tag_file_checksums), SEEK_SET);
         std::fwrite(&checksum_delta, sizeof(checksum_delta), 1, f);
         std::fclose(f);
+
+        oprintf_success("Successfully forged CRC32 to 0x%08X", final_crc);
     }
     else {
+        // Calculate CRC32
         final_crc = calculate_map_crc(data.get(), size);
+        oprintf("0x%08X\n", final_crc);
     }
 
-    // Calculate CRC32
-    printf("%08X\n", final_crc);
     return EXIT_SUCCESS;
 }
