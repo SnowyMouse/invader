ee93fbc20699da2fbb9fe788b18ec6f98916d88e
Aerocatia <aerocatia@tutanota.com>
2025-05-31 09:14:15 +1200

    invader-crc - add option to fix the header crc

--- src/crc/crc.cpp
+++ src/crc/crc.cpp
@@ -11,6 +11,8 @@ extern "C" {
     #include "crc32.h"
 }
 
+using namespace Invader;
+
 static std::uint32_t read_str32(const char *err, const char *s) {
     // Make sure it starts with '0x'
     if(std::strncmp(s, "0x", 2) != 0) {
@@ -37,31 +39,58 @@ static std::uint32_t read_str32(const char *err, const char *s) {
     return static_cast<std::uint32_t>(std::strtoul(s + 2, nullptr, 16));
 }
 
+static void update_cache_file(const auto file, bool demo_header, std::uint32_t crc32, std::optional<std::size_t> tag_data_offset = std::nullopt, std::optional<std::uint32_t> delta = std::nullopt) {
+    std::size_t crc32_offset = offsetof(HEK::CacheFileHeader, crc32);
+    if(demo_header) {
+        crc32_offset = offsetof(HEK::CacheFileDemoHeader, crc32);
+    }
+
+    std::FILE *f = std::fopen(file, "rb+");
+    if(!f) {
+        eprintf_error("Failed to open %s", file);
+        std::exit(EXIT_FAILURE);
+    }
+
+    // Write header crc32
+    std::fseek(f, crc32_offset, SEEK_SET);
+    std::fwrite(&crc32, sizeof(crc32), 1, f);
+
+    // Write delta to tag header if we need to
+    if(delta) {
+        std::fseek(f, tag_data_offset.value() + offsetof(HEK::CacheFileTagDataHeader, tag_file_checksums), SEEK_SET);
+        std::fwrite(&delta.value(), sizeof(delta.value()), 1, f);
+    }
+    std::fclose(f);
+}
+
 int main(int argc, const char **argv) {
     set_up_color_term();
 
-    using namespace Invader;
-
     const CommandLineOption options[] {
         CommandLineOption::from_preset(CommandLineOption::PRESET_COMMAND_LINE_OPTION_INFO),
-        CommandLineOption("forge", 'f', 1, "Forge the CRC32 value of the map.", "<crc>")
+        CommandLineOption("forge", 'c', 1, "Forge the CRC32 value of the map.", "<crc>"),
+        CommandLineOption("fix-header", 'f', 0, "Set the CRC32 field in the cache file header to the current calculated value if mismatched. This is implied when using --forge")
     };
 
     static constexpr char DESCRIPTION[] = "Inspect or forge the CRC32 of a cache file.";
     static constexpr char USAGE[] = "[options] <map>";
 
     struct CrcOptions {
+        bool fix_header = false;
         std::optional<std::uint32_t> forge_crc;
     } crc_options;
 
     auto remaining_arguments = CommandLineOption::parse_arguments<CrcOptions &>(argc, argv, options, USAGE, DESCRIPTION, 1, 1, crc_options, [](char opt, const auto &arguments, auto &crc_options) {
         switch(opt) {
+            case 'c':
+                crc_options.forge_crc = read_str32("Invalid CRC32", arguments[0]);
+                break;
+            case 'f':
+                crc_options.fix_header = true;
+                break;
             case 'i':
                 show_version_info();
                 std::exit(EXIT_SUCCESS);
-                break;
-            case 'f':
-                crc_options.forge_crc = read_str32("Invalid CRC32", arguments[0]);
                 break;
         }
     });
@@ -96,11 +125,13 @@ int main(int argc, const char **argv) {
     bool has_demo_header = false;
     HEK::CacheFileEngine cache_version;
     HEK::CacheFileHeader header;
+    std::uint32_t header_crc;
     std::memcpy(static_cast<void *>(&header), data.get(), sizeof(HEK::CacheFileHeader));
     if(header.valid()) {
         tag_data_offset = header.tag_data_offset.read();
         tag_data_size = header.tag_data_size.read();
         cache_version = header.engine.read();
+        header_crc = header.crc32.read();
     }
     else {
         // Try Demo?
@@ -114,6 +145,7 @@ int main(int argc, const char **argv) {
         tag_data_offset = demo_header.tag_data_offset.read();
         tag_data_size = demo_header.tag_data_size.read();
         cache_version = demo_header.engine.read();
+        header_crc = demo_header.crc32.read();
     }
 
     if(cache_version == HEK::CACHE_FILE_XBOX) {
@@ -132,33 +164,24 @@ int main(int argc, const char **argv) {
         // Forge the CRC32 to a new value
         std::uint32_t checksum_delta;
         final_crc = calculate_map_crc(data.get(), size, &crc_options.forge_crc.value(), &checksum_delta);
-
-        std::size_t crc32_offset = offsetof(HEK::CacheFileHeader, crc32);
-        if(has_demo_header) {
-            crc32_offset = offsetof(HEK::CacheFileDemoHeader, crc32);
-        }
-
-        f = std::fopen(file, "rb+");
-        if(!f) {
-            eprintf_error("Failed to open %s", file);
-            return EXIT_FAILURE;
-        }
-
-        // Write header crc32
-        std::fseek(f, crc32_offset, SEEK_SET);
-        std::fwrite(&final_crc, sizeof(final_crc), 1, f);
-
-        // Write delta to tag header
-        std::fseek(f, tag_data_offset + offsetof(HEK::CacheFileTagDataHeader, tag_file_checksums), SEEK_SET);
-        std::fwrite(&checksum_delta, sizeof(checksum_delta), 1, f);
-        std::fclose(f);
-
+        update_cache_file(file, has_demo_header, final_crc, tag_data_offset, checksum_delta);
         oprintf_success("Successfully forged CRC32 to 0x%08X", final_crc);
     }
     else {
         // Calculate CRC32
         final_crc = calculate_map_crc(data.get(), size);
-        oprintf("0x%08X\n", final_crc);
+        if(crc_options.fix_header) {
+            if(final_crc != header_crc) {
+                update_cache_file(file, has_demo_header, final_crc);
+                oprintf_success("Mismatched header CRC32 (0x%08X) was set to 0x%08X", header_crc, final_crc);
+            }
+            else {
+                oprintf("CRC32 0x%08X matches header (nothing to fix)\n", final_crc);
+            }
+        }
+        else {
+            oprintf("0x%08X\n", final_crc);
+        }
     }
 
     return EXIT_SUCCESS;
