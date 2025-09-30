a6a2e99059fc8cce86873217cb609fd88714bed1
Aerocatia <aerocatia@tutanota.com>
2025-09-30 19:04:41 +1300

    tweak for closer tool precision

--- src/tag/parser/compile/actor.cpp
+++ src/tag/parser/compile/actor.cpp
@@ -6,8 +6,8 @@
 namespace Invader::Parser {
     void Actor::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t struct_offset) {
         auto *actor = reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + struct_offset);
-        
-        #define SET_INVERTED_VALUE(normal,inverted) { if(normal.read() > 0.0F) { inverted = TICK_RATE_RECIPROCOL / normal.read(); } else { inverted = 0.0F; } }
+
+        #define SET_INVERTED_VALUE(normal,inverted) { if(normal.read() > 0.0F) { inverted = 1.0F / (normal.read() * TICK_RATE); } else { inverted = 0.0F; } }
 
         SET_INVERTED_VALUE(actor->combat_perception_time, actor->inverse_combat_perception_time);
         SET_INVERTED_VALUE(actor->guard_perception_time, actor->inverse_guard_perception_time);
@@ -19,7 +19,7 @@ namespace Invader::Parser {
         actor->cosine_maximum_looking_deviation.yaw = std::cos(actor->maximum_looking_deviation.yaw);
         actor->cosine_maximum_looking_deviation.pitch = std::cos(actor->maximum_looking_deviation.pitch);
     }
-    
+
     void ActorVariant::post_compile(BuildWorkload &workload, std::size_t, std::size_t struct_index, std::size_t struct_offset) {
         auto *actor_variant = reinterpret_cast<struct_little *>(workload.structs[struct_index].data.data() + struct_offset);
         actor_variant->grenade_velocity = actor_variant->grenade_velocity.read() * TICK_RATE_RECIPROCOL;
