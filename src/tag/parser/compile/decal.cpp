56d7221b53f4ef07321d36e6c6c90845bb41364b
Aerocatia <aerocatia@tutanota.com>
2026-04-03 04:57:13 +1300

    error if you try to use the decal fix with this invader version

--- src/tag/parser/compile/decal.cpp
+++ src/tag/parser/compile/decal.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <invader/tag/parser/parser.hpp>
+#include <invader/build/build_workload.hpp>
 
 namespace Invader::Parser {
     void Decal::postprocess_hek_data() {
@@ -27,4 +28,13 @@ namespace Invader::Parser {
 
         this->lifetime.to = std::max(this->lifetime.from, this->lifetime.to);
     }
+
+    void Decal::pre_compile(BuildWorkload &workload, std::size_t tag_index, std::size_t, std::size_t) {
+        // This will be available in the ringhopper implementation instead.
+        if(flags & HEK::DecalFlagsFlag::DECAL_FLAGS_FLAG_SPRITE_SCALE_BUG_FIX) {
+            workload.report_error(BuildWorkload::ErrorType::ERROR_TYPE_FATAL_ERROR, "The \"sprite scale bug fix\" option is not supported by this version of invader", tag_index);
+            throw InvalidTagDataException();
+        }
+    }
+
 }
